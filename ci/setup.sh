7581e1f549d2a2309c28ade0db10858359145ca2
Fabrice Reix <fabrice.reix@orange.com>
2020-09-11 10:52:53 +0200

    Split/Refacto Travis job

---
+++ ci/setup.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -e
+
+#ROOT_DIR=$(pwd)
+
+rustup component add clippy
+cargo clippy --version
+cargo install cargo-deb
+
+# Python/Flask
+sudo apt-get install python3-pip
+which python3
+python3 -V
+which pip3
+pip3 install Flask
+(cd integration && python3 server.py&)
+
+
+# Mitmproxy
+wget https://snapshots.mitmproxy.org/5.2/mitmproxy-5.2-linux.tar.gz -O - | tar -xz
+./mitmdump &
+
+sleep 2
+netstat -an | grep LISTEN | grep -E '8000|8080'
+
+
