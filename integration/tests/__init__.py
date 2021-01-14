13e8d5fe7b039c5eb58c9299ac8b8a86d10c404b
Fabrice Reix <fabrice.reix@orange.com>
2021-01-14 16:14:04 +0100

    Support for windows

--- integration/tests/__init__.py
+++ integration/tests/__init__.py
@@ -9,7 +9,7 @@ current_dir = os.path.basename(os.path.dirname(__file__))
 for python_file in glob.glob(current_dir + '/*.py'):
     if python_file.endswith('__init__.py'):
         continue
-    module_name = python_file.split('.')[0].replace('/', '.')
+    module_name = python_file.split('.')[0].replace(os.path.sep, '.')
     print('loading %s' % module_name)
     try:
         importlib.import_module(module_name)
