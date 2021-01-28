797788c571b2c326b5e943764c323ee31589112a
Fabrice Reix <fabrice.reix@orange.com>
2021-01-28 11:49:21 +0100

    Tests hurl files in order

--- integration/integration.py
+++ integration/integration.py
@@ -8,14 +8,14 @@ import test_hurl
 
 def main():
     # Static run (without server)
-    [test_echo.test(f) for f in glob.glob('tests/*.hurl') + glob.glob('tests_error_lint/*.hurl')]
-    [test_format.test('json', f) for f in glob.glob('tests/*.hurl')]
-    [test_format.test('html', f) for f in glob.glob('tests/*.hurl')]
-    [test_lint.test(f) for f in glob.glob('tests_error_lint/*.hurl')]
-    [test_hurl.test(f) for f in glob.glob('tests_error_parser/*.hurl')]
+    [test_echo.test(f) for f in sorted(glob.glob('tests/*.hurl') + glob.glob('tests_error_lint/*.hurl'))]
+    [test_format.test('json', f) for f in sorted(glob.glob('tests/*.hurl'))]
+    [test_format.test('html', f) for f in sorted(glob.glob('tests/*.hurl'))]
+    [test_lint.test(f) for f in sorted(glob.glob('tests_error_lint/*.hurl'))]
+    [test_hurl.test(f) for f in sorted(glob.glob('tests_error_parser/*.hurl'))]
 
     # Dynamic run (with server)
-    [test_hurl.test(f) for f in glob.glob('tests/*.hurl')]
+    [test_hurl.test(f) for f in sorted(glob.glob('tests/*.hurl'))]
 
     print('test integration ok!')
 
