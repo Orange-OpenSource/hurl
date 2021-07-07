1b30968c9560f64a8632f9dc555060accd9dfa9a
Fabrice Reix <fabrice.reix@orange.com>
2021-07-07 15:44:48 +0200

    Support startsWith and contains with bytes

--- integration/tests/bytes.py
+++ integration/tests/bytes.py
@@ -6,7 +6,7 @@ from io import BytesIO
 @app.route('/bytes')
 def bytes():
     result = BytesIO()
-    result.write(b'\xff')
+    result.write(b'\x01\x02\x03')
     data = result.getvalue()
     resp = make_response(data)
     resp.content_type = 'application/octet-stream'
