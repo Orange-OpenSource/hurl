809609a442ae9f471db6e63dc8deb506e12960dc
Fabrice Reix <fabrice.reix@orange.com>
2021-11-08 23:06:40 +0100

    Set variables from HURL_ environment variables

---
+++ integration/tests/env_var.profile
@@ -0,0 +1,1 @@
+HURL_name=Bob
--- packages/hurl/src/cli/mod.rs
+++ packages/hurl/src/cli/mod.rs
@@ -28,6 +28,7 @@ pub use self::options::output_color;
 pub use self::options::parse_options;
 pub use self::options::{CliOptions, OutputType};
 pub use self::variables::parse as parse_variable;
+pub use self::variables::parse_value as parse_variable_value;
 
 mod fs;
 pub mod interactive;
