c6bf58030f5f82379718bad0a10c7728f0f2a540
Fabrice Reix <fabrice.reix@orange.com>
2021-11-08 11:09:09 +0100

    Rename option --html

--- integration/report-html.sh
+++ integration/report-html.sh
@@ -4,13 +4,13 @@
 set +e
 rm -rf report/*
 find tests -name "*.hurl" | sort | while read -r hurl_file; do
-    options=("--json report/tests.json" "--html report/html" "--output /dev/null")
+    options=("--json report/tests.json" "--report-html report/html" "--output /dev/null")
     if test -f "${hurl_file%.*}.options"; then
         options+=("$(cat "${hurl_file%.*}.options")")
     fi
     cmd="hurl $hurl_file ${options[*]}"
     echo "$cmd"
-    $cmd >/tmp/test.stdout 2>/tmp/test.stderr
+    $cmd  
     exit_code=$?
 
     if [ "$exit_code" -gt 4 ]; then
