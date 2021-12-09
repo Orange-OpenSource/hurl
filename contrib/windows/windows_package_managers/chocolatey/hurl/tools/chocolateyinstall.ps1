c2f45db5f9f68f6028cd62bb2795b415a9cf441b
lepapareil <filipe.pinto@orange.com>
2021-12-09 15:05:55 +0100

    Convert contrib windows chocolatey files to templates

--- contrib/windows/windows_package_managers/chocolatey/hurl/tools/chocolateyinstall.ps1
+++ contrib/windows/windows_package_managers/chocolatey/hurl/tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@ $SoftwareName  = 'hurl'
 $HashArguments = @{
   PackageName = $env:ChocolateyPackageName
   UnzipLocation = $(Split-Path -Parent $MyInvocation.MyCommand.Definition)
-  Url64 = 'https://github.com/Orange-OpenSource/hurl/releases/download/1.4.0/hurl-1.4.0-win64.zip'
-  Checksum64 = '78255BB838095A1015679F92189074C1162EDD51B5EDC1DDA7B863BA7304C4B5'
+  Url64 = 'https://github.com/Orange-OpenSource/hurl/releases/download/{version}/hurl-{version}-win64.zip'
+  Checksum64 = '{Checksum64}'
   ChecksumType64 = 'sha256'
 }
 
