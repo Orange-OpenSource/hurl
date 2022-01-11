a3a500baae944530af3665a045f690ba4c3d7cbb
jcamiel <jeanchristophe.amiel@orange.com>
2022-01-11 17:41:34 +0100

    Update copyright for 2022

--- packages/hurl/src/report/mod.rs
+++ packages/hurl/src/report/mod.rs
@@ -1,5 +1,5 @@
 /*
- * hurl (https://hurl.dev)
+ * Hurl (https://hurl.dev)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
