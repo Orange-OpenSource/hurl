2b890cf5e0ef0a37b091ea76f92b3ceb4322561a
jcamiel <jeanchristophe.amiel@orange.com>
2022-01-11 17:41:34 +0100

    Update copyright for 2022

--- packages/hurl_core/src/parser/expr.rs
+++ packages/hurl_core/src/parser/expr.rs
@@ -1,6 +1,6 @@
 /*
- * hurl (https://hurl.dev)
- * Copyright (C) 2020 Orange
+ * Hurl (https://hurl.dev)
+ * Copyright (C) 2022 Orange
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
