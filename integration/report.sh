227228e1bbe74f13f64284668a1b53e26016fa7b
Fabrice Reix <fabrice.reix@orange.com>
2022-02-05 00:08:55 +0100

    Check number of tests is valid

--- integration/report.sh
+++ integration/report.sh
@@ -6,31 +6,53 @@ rm -rf build
 mkdir build
 export HURL_name=Bob
 
-find tests -name "*.hurl" | sort | while read -r hurl_file; do
-    options=("--report-html build/html" "--report-junit build/tests.xml" "--json" )
-    if test -f "${hurl_file%.*}.options"; then
-        options+=("$(< "${hurl_file%.*}.options" tr '\n' ' ') ")
-    fi
-    cmd="hurl $hurl_file ${options[*]}"
-    echo "$cmd"
-    $cmd >> "build/tests.json"
-    exit_code=$?
+# read options
+# params:
+#     options_file   optional filename
+function read_options() {
+  file=$1
+  if test -f "$file"; then
+    while read -r option ; do
+     if [[ $option =~ " " ]]; then
+        echo -n " '$option'"
+     else
+         echo -n " $option"
+     fi
+     done < "$options_file"
+  fi
 
+}
+
+find tests -name "*.hurl" | sort | while read -r hurl_file; do
+    options_file="${hurl_file%.*}.options"
+    options="--report-html build/html --report-junit build/tests.xml --json $(read_options "$options_file")"
+    cmd="hurl $hurl_file $options"
+    echo "$cmd"
+    echo "$cmd" | sh >> "build/tests.json"
+    exit_code=$?
     if [[ "$exit_code" != 0 && "$exit_code" != 3 && "$exit_code" != 4 ]]; then
 	     echo "unexpected exit code $exit_code"
 	     exit 1
     fi
 done
 
+set -e
 
-total=$(ls tests/*.hurl | wc -l)
-total_in_json=$(cat build/tests.json | wc -l)
-total_in_xml=$(cat build/tests.xml | xmllint --xpath '//testcase' - | grep 'testcase id' | wc -l)
+total=$(find tests -name '*.hurl' | wc -l)
+total_in_json=$( wc -l < build/tests.json)
+total_in_xml=$(xmllint --xpath '//testcase' - < build/tests.xml| grep -c 'testcase id')
 
-# Do not fail yet
-echo "Total Number of tests"
-echo "Hurl files:    $total"
-echo "Tests in JSON: $total_in_json"
-echo "Tests in XML:  $total_in_xml"
+echo "Total Number of tests: $total"
+
+if [[ "$total_in_json" -ne "$total" ]] ; then
+  echo "Number of tests in JSON do not match!"
+  exit 1
+fi
+if [[ "$total_in_xml" -ne "$total" ]] ; then
+  echo "Number of tests in XML do not match!"
+  exit 1
+fi
+
+
 
 
