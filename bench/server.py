745fb1d193744336d5186a240305743599a69f32
Fabrice Reix <fabrice.reix@orange.com>
2022-02-05 11:13:49 +0100

    Format all the python files in the project

--- bench/server.py
+++ bench/server.py
@@ -3,10 +3,10 @@ from flask import request
 
 app = Flask(__name__)
 
-@app.route('/hello')
+
+@app.route("/hello")
 def hello():
-    return 'Hello World!'
-
-app.run(host='0.0.0.0', port=8000)
+    return "Hello World!"
 
 
+app.run(host="0.0.0.0", port=8000)
