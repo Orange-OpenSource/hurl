89e565a01bcbe012c2f3ecf8a9ee2b128997eede
Fabrice Reix <fabrice.reix@orange.com>
2022-02-05 11:13:49 +0100

    Format all the python files in the project

--- ci/gen_manpage.py
+++ ci/gen_manpage.py
@@ -5,12 +5,15 @@ from datetime import date
 
 
 def header(version, date):
-    return '.TH hurl 1 "%s" "hurl %s" " Hurl Manual"' % (date.strftime("%d %b %Y"), version)
+    return '.TH hurl 1 "%s" "hurl %s" " Hurl Manual"' % (
+        date.strftime("%d %b %Y"),
+        version,
+    )
 
 
 def version():
     p = re.compile('version = "(.*)"')
-    for line in open('packages/hurl/Cargo.toml', 'r').readlines():
+    for line in open("packages/hurl/Cargo.toml", "r").readlines():
         m = p.match(line)
         if m:
             return m.group(1)
@@ -18,45 +21,43 @@ def version():
 
 
 def process_code_block(s):
-    output = ''
+    output = ""
     indent = False
-    for line in s.split('\n'):
-        if indent and line.startswith('```'):
+    for line in s.split("\n"):
+        if indent and line.startswith("```"):
             indent = False
-        elif not indent and line.startswith('```'):
+        elif not indent and line.startswith("```"):
             indent = True
         else:
-            if line != '':
+            if line != "":
                 if indent:
-                    output += '    '
+                    output += "    "
                 output += line
-            output += '\n'
+            output += "\n"
 
     return output
-    #p.sub('\\\\f[C]\\1\\\\f[R]', s)
+    # p.sub('\\\\f[C]\\1\\\\f[R]', s)
 
 
 def convert_md(s):
 
-     p = re.compile('^###\s+(.*)')
-     s = p.sub('.IP "\\1"', s)
+    p = re.compile("^###\s+(.*)")
+    s = p.sub('.IP "\\1"', s)
 
-     p = re.compile('^##')
-     s = p.sub('.SH', s)
+    p = re.compile("^##")
+    s = p.sub(".SH", s)
 
+    p = re.compile("\*\*(.*)\*\*\s+")
+    s = p.sub(".B \\1\n", s)
 
-     p = re.compile('\*\*(.*)\*\*\s+')
-     s = p.sub('.B \\1\n', s)
+    # Remove link Text
+    p = re.compile("\[(.*)\]\(.*\)")
+    s = p.sub("\\\\fI\\1\\\\fP", s)
 
-
-     # Remove link Text
-     p = re.compile('\[(.*)\]\(.*\)')
-     s = p.sub('\\\\fI\\1\\\\fP', s)
-
-     # Remove local anchor
-     p = re.compile('{#.*}')
-     s = p.sub('', s)
-     return s
+    # Remove local anchor
+    p = re.compile("{#.*}")
+    s = p.sub("", s)
+    return s
 
 
 def main():
@@ -64,11 +65,11 @@ def main():
     data = open(input_file).readlines()
     print(header(version(), date.today()))
 
-    s = ''.join([convert_md(line) for line in data])
+    s = "".join([convert_md(line) for line in data])
 
     s = process_code_block(s)
     print(s)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
