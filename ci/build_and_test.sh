309f7de1c69d647fdce0b874d87671bd92d318b9
jcamiel <jeanchristophe.amiel@orange.com>
2022-03-21 19:24:06 +0100

    Test build are done with --release and --locked.
    Hurl and curl version are outputed juste after build.

--- ci/build_and_test.sh
+++ ci/build_and_test.sh
@@ -2,9 +2,12 @@
 set -e
 
 echo "----- build -----"
+# Directive for ShellCheck SC1090:
 # shellcheck source=/dev/null
 source ~/.cargo/env
-cargo build --verbose
+cargo build --release --verbose --locked
+target/release/hurl --version
+curl --version
 
 ci/test_prerequisites.sh
 
