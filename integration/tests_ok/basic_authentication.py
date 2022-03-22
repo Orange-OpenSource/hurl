19a4a6042be2dcab8375a38b8a9db1926bf28355
Fabrice Reix <fabrice.reix@orange.com>
2022-03-22 20:45:18 +0100

    Support @ for the username in [BasicAuth] section

--- integration/tests_ok/basic_authentication.py
+++ integration/tests_ok/basic_authentication.py
@@ -4,5 +4,5 @@ from flask import request
 
 @app.route("/basic-authentication")
 def basic_authentication():
-    assert request.headers["Authorization"] == "Basic Ym9iOnNlY3JldA=="
+    assert request.headers["Authorization"] == "Basic Ym9iQGVtYWlsLmNvbTpzZWNyZXQ="
     return "You are authenticated"
--- integration/tests_ok/basic_authentication_per_request.py
+++ integration/tests_ok/basic_authentication_per_request.py
@@ -4,5 +4,6 @@ from flask import request
 
 @app.route("/basic-authentication-per-request")
 def basic_authentication_per_request():
-    assert request.headers["Authorization"] == "Basic Ym9iOnNlY3JldA=="
+    print(request.headers["Authorization"])
+    assert request.headers["Authorization"] == "Basic Ym9iQGVtYWlsLmNvbTpzZWNyZXQ="
     return "You are authenticated"
