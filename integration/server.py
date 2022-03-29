9c567900cbfd50effb0e0d60ce40ebc07b15b6b9
jcamiel <jeanchristophe.amiel@orange.com>
2022-03-29 10:00:57 +0200

    Configure Flask to use HTTP/1.0 in integration tests.

--- integration/server.py
+++ integration/server.py
@@ -1,5 +1,7 @@
 from app import app, load_tests
+from werkzeug.serving import WSGIRequestHandler
 
 if __name__ == "__main__":
     load_tests()
+    WSGIRequestHandler.protocol_version = "HTTP/1.0"
     app.run(host="0.0.0.0", port=8000)
--- integration/ssl/server.py
+++ integration/ssl/server.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 from flask import Flask
+from werkzeug.serving import WSGIRequestHandler
 import os
 
 app = Flask(__name__)
@@ -12,4 +13,5 @@ def hello():
 
 if __name__ == "__main__":
     ssl_dir = os.path.dirname(os.path.realpath(__file__))
+    WSGIRequestHandler.protocol_version = "HTTP/1.0"
     app.run(port=8001, ssl_context=(ssl_dir + "/cert.pem", ssl_dir + "/key.pem"))
