bf3ed1dd669f929dd0b2a4059746175a65edfd68
jcamiel <jeanchristophe.amiel@orange.com>
2022-05-19 11:41:52 +0200

    Move docs.

--- ci/man.sh
+++ ci/man.sh
@@ -3,8 +3,8 @@ set -e
 
 rm -rf target/man
 mkdir -p target/man
-ci/gen_manpage.py docs/hurl.md > target/man/hurl.1
-ci/gen_manpage.py docs/hurlfmt.md > target/man/hurlfmt.1
+ci/gen_manpage.py docs/man/hurl.md > target/man/hurl.1
+ci/gen_manpage.py docs/man/hurlfmt.md > target/man/hurlfmt.1
 
 gzip -n -9 target/man/hurl.1
 gzip -n -9 target/man/hurlfmt.1
