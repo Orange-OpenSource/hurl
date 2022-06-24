fb057872fab8f34deac6f47a8cad48d4d54073a5
jcamiel <jeanchristophe.amiel@orange.com>
2022-06-24 13:20:30 +0200

    Reactivate ArchLinux integration tests.
    Following https://github.com/curl/curl/issues/8559 fix, ArchLinux tests should be ok now.

--- ci/build_and_test.sh
+++ ci/build_and_test.sh
@@ -11,12 +11,6 @@ curl --version
 
 ci/test_prerequisites.sh
 
-# current bug with curl to be fixed
-# https://github.com/curl/curl/issues/8559
-if test -f /etc/arch-release; then
-   exit 0
-fi
-
 echo "----- unit tests  -----"
 cargo test --features strict --tests
 
