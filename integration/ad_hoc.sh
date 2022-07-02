1017dffdf135c2242da2b72d70d625f6463820a0
Fabrice Reix <fabrice.reix@orange.com>
2022-07-02 06:08:26 +0200

    Add additional script for ad-hoc tests

---
+++ integration/ad_hoc.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# add more tests
+# that can be easily added in tests_ok/ and tests_failed/
+
+echo "Check file not found error"
+actual=$(hurl does_not_exist.hurl 2>&1)
+expected="error: hurl: cannot access 'does_not_exist.hurl': No such file or directory"
+if [ "$actual" != "$expected" ]; then
+    echo "Error differs:"
+    echo "actual: $actual"
+    echo "expected: $expected"
+    exit 1
+fi
+
+
