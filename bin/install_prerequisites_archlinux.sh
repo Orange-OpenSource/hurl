eb9c46a9d4aeadebed49e4c024458c3faaa6e210
jcamiel <jeanchristophe.amiel@orange.com>
2022-08-05 13:52:13 +0200

    Patches chrono 0.4.20 regression on ArchLinux / Alpine docker image.

--- bin/install_prerequisites_archlinux.sh
+++ bin/install_prerequisites_archlinux.sh
@@ -3,5 +3,11 @@ set -eu
 echo "----- install prerequisite packages -----"
 pacman -Syy --noconfirm
 pacman -Sy --noconfirm curl icu base-devel libxml2 python3 glibc
+
+# FIXME: chrono 0.4.20 basically requires one to set up a correct timezone
+# We set /etc/localtime in a Docker context
+# see https://github.com/chronotope/chrono/issues/755
+ln -s /usr/share/zoneinfo/Europe/Paris /etc/localtime
+
 curl -O https://bootstrap.pypa.io/get-pip.py
 python3 get-pip.py
