2d9e860ff6327801e2574dc1ece75521999f5fe4
jcamiel <jeanchristophe.amiel@orange.com>
2022-08-17 18:55:39 +0200

    Add Netscape cookie format comment.

--- packages/hurl/src/http/core.rs
+++ packages/hurl/src/http/core.rs
@@ -44,6 +44,29 @@ pub struct Param {
 }
 
 impl fmt::Display for Cookie {
+    /// Formats this cookie using Netscape cookie format.
+    ///
+    /// http://www.cookiecentral.com/faq/#3.5
+    ///
+    /// > The layout of Netscape's cookies.txt file is such that each line contains one name-value
+    /// > pair. An example cookies.txt file may have an entry that looks like this:
+    /// >
+    /// > `.netscape.com     TRUE   /  FALSE  946684799   NETSCAPE_ID  100103`
+    /// >
+    /// > Each line represents a single piece of stored information. A tab is inserted between each
+    /// > of the fields.
+    /// > From left-to-right, here is what each field represents:
+    /// > - domain - The domain that created AND that can read the variable.
+    /// > - flag - A TRUE/FALSE value indicating if all machines within a given domain can access
+    /// > the variable. This value is set automatically by the browser, depending on the value you
+    /// > set for domain.
+    /// > - path - The path within the domain that the variable is valid for.
+    /// > - secure - A TRUE/FALSE value indicating if a secure connection with the domain is
+    /// > needed to access the variable.
+    /// > - expiration - The UNIX time that the variable will expire on. UNIX time is defined as the
+    /// > - number of seconds since Jan 1, 1970 00:00:00 GMT.
+    /// > - name - The name of the variable.
+    /// > - value - The value of the variable.
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(
             f,
