88ad6a3d19c58afa829f6334b806c6956432cd8a
jcamiel <jeanchristophe.amiel@orange.com>
2022-09-06 15:51:51 +0200

    Reduces Hurl release binary size.

--- bin/release/release.sh
+++ bin/release/release.sh
@@ -4,8 +4,6 @@ set -e
 PATH="$HOME"/.cargo/bin:$PATH
 export PATH
 cargo build --release --verbose --locked
-strip target/release/hurl
-strip target/release/hurlfmt
 
 target/release/hurl --version
 
