e590b6eedae473cc7b2276fb9f068f3f958ea2e5
lepapareil <filipe.pinto@orange.com>
2022-09-19 14:20:14 +0200

    Clean ci integration bindings

--- bin/test/test_prerequisites.sh
+++ bin/test/test_prerequisites.sh
@@ -6,6 +6,6 @@ echo "----- start servers -----"
 cd integration
 python3 server.py >server.log 2>&1 &
 python3 ssl/server.py >server-ssl.log 2>&1 &
-mitmdump -p 8888 --modify-header "/From-Proxy/Hello" >mitmdump.log 2>&1 &
+mitmdump --listen-host 127.0.0.1 --listen-port 8888 --modify-header "/From-Proxy/Hello" >mitmdump.log 2>&1 &
 
 
--- integration/server.py
+++ integration/server.py
@@ -2,4 +2,4 @@ from app import app, load_tests
 
 if __name__ == "__main__":
     load_tests()
-    app.run(host="0.0.0.0", port=8000)
+    app.run(host="127.0.0.1", port=8000)
