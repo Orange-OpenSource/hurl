c8550b673c06845f8a48a2f4bd6910213d502ebd
lepapareil <filipe.pinto@orange.com>
2022-10-04 15:31:38 +0200

    Run test integ on deb package

--- bin/release/release.sh
+++ bin/release/release.sh
@@ -5,5 +5,7 @@ PATH="$HOME"/.cargo/bin:$PATH
 export PATH
 cargo build --release --verbose --locked
 
-target/release/hurl --version
-
+# Return PATH var to parent shell
+package_dir="$(cd target/release ; pwd)"
+echo "Run this if you want to use fresh builded hurl package:"
+echo "  export PATH=$package_dir:$PATH"
