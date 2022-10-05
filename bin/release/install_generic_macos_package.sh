4e8f772759da968b6d811ec8e577065abcaab17e
lepapareil <filipe.pinto@orange.com>
2022-10-05 15:45:03 +0200

    Run test integ on generic macos package

---
+++ bin/release/install_generic_macos_package.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+set -e
+
+echo "----- install generic macos package -----"
+
+# install
+generic_macos_package=$(ls target/upload/hurl-*-x86_64-macos.tar.gz)
+
+install_dir="/tmp/hurl-generic-macos"
+mkdir -p "${install_dir}"
+tar xvf "${generic_macos_package}" -C "${install_dir}" --strip-components=1
+
+# Return PATH var to parent shell
+echo "Run this if you want to use fresh builded hurl package:"
+echo "  export PATH=${install_dir}:$PATH"
