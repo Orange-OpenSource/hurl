43487b0f1a6a1f49a1e6b59e2fd486d431c0bd55
Luke Mondy <work@luke.malondy.net>
2022-12-06 09:31:20 +1100

    Add integration tests for htmlEscape and htmlUnescape filters

--- integration/tests_ok/filter.py
+++ integration/tests_ok/filter.py
@@ -8,6 +8,8 @@ def filter():
   "message": "Hello Bob!",
   "url": "https://mozilla.org/?x=шеллы",
   "encoded_url": "https://mozilla.org/?x=%D1%88%D0%B5%D0%BB%D0%BB%D1%8B",
+  "html": "a > b && a < c",
+  "encoded_html": "a &gt; b &amp;&amp; a &lt; c",
   "id": "123",
   "score": 1.6
 }"""
