c52eb098cb78308b1c3b571bab4e3b1cbbf84ff8
lepapareil <filipe.pinto@orange.com>
2022-12-14 10:00:47 +0100

    Add shell scripts shebang iand error handling check

--- bin/check/ad_hoc.sh
+++ bin/check/ad_hoc.sh
@@ -5,12 +5,37 @@ color_red=$(echo -e "\033[1;31m")
 color_reset=$(echo -e "\033[0m")
 errors_count=0
 
-# Check that Rust source files contains Orange Copyright
-find packages -name "*.rs" | while read -r rust_file; do
-    if ! grep -q "Copyright (C) 2022 Orange" "$rust_file"; then
-        echo "Missing copyright in $rust_file"
-	exit 1
-    fi
-done
+# Check *.rs Orange Copyright
+echo "------------------------------------------------------------------------------------------"
+find packages -type f -name "*.rs" | \
+    while read -r rust_file ; do
+        if [ $(grep -c "Copyright (C) 2022 Orange" "$rust_file" || true) -eq 0 ] ; then
+            echo "Missing [Copyright (C) 2022 Orange] in ${color_red}${rust_file}${color_reset}"
+            ((errors_count++))
+        fi
+    done
 
+# Check *sh bash shebang at line 1
+echo "------------------------------------------------------------------------------------------"
+find . -type f -name "*.sh" | \
+    while read -r script ; do
+        if [ $(head -1 "$script" | grep -c "#!/bin/bash" || true) -eq 0 ] ; then
+            echo "Missing [#!/bin/bash] shebang in ${color_red}${script}${color_reset}"
+            ((errors_count++))
+        fi
+    done
 
+# Check *sh error handling at line 2
+echo "------------------------------------------------------------------------------------------"
+find . -type f -name "*.sh" | \
+    while read -r script ; do
+        if [ $(head -2 "$script" | tail -1 | grep -c "set -Eeuo pipefail" || true) -eq 0 ] ; then
+            echo "Missing [set -Eeuo pipefail] in ${color_red}${script}${color_reset}"
+            ((errors_count++))
+        fi
+    done
+
+# Control errors count
+if [ "${errors_count}" -gt 0 ] ; then
+    exit 1
+fi
--- bin/update_crates.sh
+++ bin/update_crates.sh
@@ -1,4 +1,6 @@
-#!/usr/bin/env bash
+#!/bin/bash
+set -Eeuo pipefail
+
 # init exit workflow
 
 # init colors
