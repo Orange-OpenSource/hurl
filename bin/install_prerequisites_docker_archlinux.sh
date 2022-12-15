d1767e198538cbc5fa0819b2211bef937174178f
lepapareil <filipe.pinto@orange.com>
2022-12-15 15:25:21 +0100

    Clean ci docker jobs

--- bin/install_prerequisites_docker_archlinux.sh
+++ bin/install_prerequisites_docker_archlinux.sh
@@ -3,6 +3,6 @@ set -Eeuo pipefail
 
 # Install packages
 pacman -Syy --noconfirm
-pacman -Sy --noconfirm bash python3 python-pip
+pacman -Sy --noconfirm bash python3 python-pip icu base-devel libxml2 glibc
 python3 -m pip install --upgrade pip --quiet
 
--- bin/install_prerequisites_docker_fedora.sh
+++ bin/install_prerequisites_docker_fedora.sh
@@ -2,6 +2,12 @@
 set -Eeuo pipefail
 
 # Install packages
-yum install -y bash python3-devel python3-pip
-python3 -m pip install --upgrade pip --quiet
+yum install -y python38 procps gcc libxml2-devel openssl-devel libcurl-devel
+alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 0
+alternatives --install /usr/bin/python3 python3 /usr/bin/python3.11 1
+alternatives --set python3 /usr/bin/python3.8
+curl https://bootstrap.pypa.io/get-pip.py -o /tmp/get-pip.py
+USER="$(whoami)"
+export USER
+python3 /tmp/get-pip.py
 
