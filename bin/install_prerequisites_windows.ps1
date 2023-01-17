f15dc66a0af4055bb41322f2598877806efde0c2
Filipe PINTO <filipe.pinto@orange.com>
2023-01-17 10:09:56 +0100

    Update vcpkg before installing libxml2 and curl

--- bin/install_prerequisites_windows.ps1
+++ bin/install_prerequisites_windows.ps1
@@ -3,6 +3,9 @@ $ErrorActionPreference = 'Stop'
 
 powershell write-host -foregroundcolor Cyan "----- install system prerequisites -----"
 
+# update vcpkg install
+git -C ((Get-command vcpkg).Source | Split-Path) pull
+
 # install libxml and libcurl[openssl]
 $vcpkg_dir=(Get-command vcpkg).Source
 
@@ -21,4 +24,3 @@ if ($LASTEXITCODE) { Throw }
 # update pip
 python -m pip install --upgrade pip --quiet
 if ($LASTEXITCODE) { Throw }
-
