4c2f2abe8d5269750423daa868b1d72f74df0ae0
Fabrice Reix <fabrice.reix@gmail.com>
2023-01-25 13:25:49 +0100

    Add log

--- bin/check/changelog.sh
+++ bin/check/changelog.sh
@@ -3,7 +3,9 @@ set -Eeuo pipefail
 # Check that issues in CHANGELOG are up-to-to-date
 
 version=$(head -1 <CHANGELOG.md| cut -d" " -f1 | cut -d'[' -f2)
+echo "version=$version"
 changelog=$(bin/release/changelog_extract.py "$version" | grep '^\* ')
+echo "get_release_note.py"
 issues=$(bin/release/get_release_note.py "$version" 2>/dev/null | grep '^\* ')
 
 if [ "$changelog" != "$issues" ];  then
