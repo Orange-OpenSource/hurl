ae86a24b84abd6944a291657b7da3302de742ec1
Filipe PINTO <filipe.pinto@orange.com>
2023-03-27 11:48:38 +0200

    Enable ci cache to accelerate vcpkg install

--- bin/install_prerequisites_windows.ps1
+++ bin/install_prerequisites_windows.ps1
@@ -9,14 +9,12 @@ git -C ((Get-command vcpkg).Source | Split-Path) pull
 # install libxml and libcurl[openssl]
 $vcpkg_dir=(Get-command vcpkg).Source
 
-if (Test-Path $vcpkg_dir\installed\x64-windows\lib\libcurl.lib) {echo "curl already installed"} else {vcpkg install curl:x64-windows}
-if ($LASTEXITCODE) { Throw }
-if (Test-Path $vcpkg_dir\installed\x64-windows\lib\libxml2.lib) {echo "libxml2 already installed"} else {vcpkg install libxml2:x64-windows}
-if ($LASTEXITCODE) { Throw }
+vcpkg install curl:x64-windows || true
+vcpkg install libxml2:x64-windows || true
 
 vcpkg update
 if ($LASTEXITCODE) { Throw }
-vcpkg upgrade
+vcpkg upgrade --no-dry-run
 if ($LASTEXITCODE) { Throw }
 vcpkg integrate install
 if ($LASTEXITCODE) { Throw }
