c2cd636f92148232933590a38fda7a59c42db608
lepapareil <filipe.pinto@orange.com>
2023-04-13 18:31:45 +0200

    Add --fail-at-end integration tests

---
+++ integration/tests_failed/option_fail_at_end_last_ko_2_files.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+set -Eeuo pipefail
+hurl --fail-at-end tests_failed/option_fail_at_end_not_last_ko.hurl tests_failed/option_fail_at_end_last_ko.hurl
---
+++ integration/tests_failed/option_fail_at_end_not_last_ko_2_files.ps1
@@ -0,0 +1,3 @@
+Set-StrictMode -Version latest
+$ErrorActionPreference = 'Stop'
+hurl --fail-at-end tests_failed/option_fail_at_end_last_ko.hurl tests_failed/option_fail_at_end_not_last_ko.hurl
