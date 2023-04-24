4550e75ae237348f10406dcc6fb277a9e2d95127
Fabrice Reix <fabrice.reix@orange.com>
2023-04-24 14:16:23 +0200

    Add more integ tests for hurlfmt --in curl

--- integration/tests_ok/import_curl.ps1
+++ integration/tests_ok/import_curl.ps1
@@ -1,4 +1,4 @@
 Set-StrictMode -Version latest
 $ErrorActionPreference = 'Stop'
-hurl tests_ok/import_curl.out > $null  # Validate expected file
+hurl --file-root . tests_ok/import_curl.out > $null  # Validate expected file
 hurlfmt --in curl tests_ok/import_curl.in
--- integration/tests_ok/import_curl.sh
+++ integration/tests_ok/import_curl.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -Eeuo pipefail
-hurl tests_ok/import_curl.out >/dev/null  # Validate expected file
+hurl --file-root . tests_ok/import_curl.out >/dev/null  # Validate expected file
 hurlfmt --in curl tests_ok/import_curl.in
