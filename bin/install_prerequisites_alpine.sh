b4ad1afddd87244cb881449d7d933bc68dc7d8bb
jcamiel <jeanchristophe.amiel@orange.com>
2023-05-10 11:15:02 +0200

    Test fixes utf8 on Alpine.

--- bin/install_prerequisites_alpine.sh
+++ bin/install_prerequisites_alpine.sh
@@ -3,7 +3,7 @@ set -Eeuo pipefail
 
 echo "----- install prerequisite packages -----"
 apk update --quiet
-apk add --quiet sudo bash sudo curl curl-dev build-base libffi-dev libxml2-dev libxml2-utils openssl-dev python3 python3-dev py3-pip cargo squid
+apk add --quiet sudo bash curl curl-dev build-base libffi-dev libxml2-dev libxml2-utils openssl-dev python3 python3-dev py3-pip squid
 python3 -m pip install --upgrade pip --quiet
 sudo squid -k shutdown || true
 sudo rm -v /dev/shm/squid*.shm >/dev/null 2>&1 || true
