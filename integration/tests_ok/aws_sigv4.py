82e5d01f8ffd8bfa1b35307f8dbdab5b28b131f0
apparentorder <github@neveragain.de>
2023-08-13 13:24:50 +0200

    reformat to satisfy "black" check

--- integration/tests_ok/aws_sigv4.py
+++ integration/tests_ok/aws_sigv4.py
@@ -1,8 +1,8 @@
 from flask import request
 from app import app
 
+
 @app.route("/aws-sigv4", methods=["POST"])
 def aws_sigv4():
-    auth = request.headers.get('Authorization')
+    auth = request.headers.get("Authorization")
     return f"Received Authorization header: {auth}"
-
