31a870117282ff3e2631127d62949830a1b7cac9
Fabrice Reix <fabrice.reix@orange.com>
2023-08-14 09:09:02 +0200

    Check that hurl has been built with libcurl dynamically

--- bin/test/test_integ.sh
+++ bin/test/test_integ.sh
@@ -10,6 +10,23 @@ command -v hurlfmt || (echo "ERROR - hurlfmt not found" ; exit 1)
 hurl --version
 hurlfmt --version
 
+# Check that hurl is dynamically linked with libcurl
+# if libcurl-dev is not installed, Hurl is built implicitly with an old static libcurl
+# https://github.com/alexcrichton/curl-rust/issues/523
+# TODO: Add MacOS
+if [[ "$(uname -s)" = "Linux*" ]]; then
+    libcurl_lib=$(ldd "$(which hurl)" | grep libcurl || test $? = 1)
+    if [ -z "$libcurl_lib" ]; then
+        echo "hurl has not been built with libcurl dynamically"
+        echo "you are probably missing the libcurl-dev package"
+        exit 1
+    else
+        echo "Using libcurl library"
+        echo "$libcurl_lib"
+    fi
+fi
+
+
 # integration tests
 cd integration
 ./hurlfmt_check.sh tests_ok/*.hurl tests_failed/*.hurl
