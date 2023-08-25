dcbcab1a0bd1394b9538e2735f44724d29734cad
apparentorder <github@neveragain.de>
2023-08-25 07:50:38 +0200

    explicitly set user-agent, as it may be part of the signature

--- integration/tests_ok/aws_sigv4.ps1
+++ integration/tests_ok/aws_sigv4.ps1
@@ -1,3 +1,3 @@
 Set-StrictMode -Version latest
 $ErrorActionPreference = 'Stop'
-hurl --user someAccessKeyId:someSecretKey tests_ok/aws_sigv4.hurl --verbose
+hurl --user-agent hurl-test --user someAccessKeyId:someSecretKey tests_ok/aws_sigv4.hurl --verbose
