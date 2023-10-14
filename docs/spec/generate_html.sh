7429aea4e9abdd2002f9bf87cd8101f884e44be2
Fabrice Reix <fabrice.reix@orange.com>
2023-10-14 20:29:36 +0200

    Update generated HTML grammar

--- docs/spec/generate_html.sh
+++ docs/spec/generate_html.sh
@@ -4,15 +4,27 @@ set -Eeuo pipefail
 # Check version
 # A specific version defines both the grammar format and the HTML output
 actual_version=$(grammar --version | cut  -d" " -f2)
-expected_version="0.1.0"
+expected_version="0.2.0"
 if [ "$actual_version" != "$expected_version" ] ; then
   echo "version mismatch"
   echo "expected: $expected_version"
   echo "actual:   $actual_version"
   exit 1
 fi
-cat <<END >hurl.grammar.html
-<link rel="stylesheet" type="text/css" href="style.css">
-END
-grammar <hurl.grammar >>hurl.grammar.html
 
+
+cat <<END >../grammar.md
+# Grammar
+
+## Definitions
+
+Short description:
+
+- operator &#124; denotes alternative,
+- operator * denotes iteration (zero or more),
+- operator + denotes iteration (one or more),
+
+## Syntax Grammar
+
+END
+grammar --section-header h3 --section-id <hurl.grammar >>../grammar.md
