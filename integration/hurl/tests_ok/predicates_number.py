0b2c069ff0d754e3d2661d811a3313b8eb83506b
Fabrice Reix <fabrice.reix@orange.com>
2023-12-13 08:13:52 +0100

    Add test float

--- integration/hurl/tests_ok/predicates_number.py
+++ integration/hurl/tests_ok/predicates_number.py
@@ -7,7 +7,9 @@ def predicates_number():
     return Response(
         """{
   "integer": 1,
-  "float": 1.0
+  "float": 1.0,
+  "float2": 0.1,
+  "float3": 0.100000000000000005
 }""",
         mimetype="application/json",
     )
