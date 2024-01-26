27dd22005f8ba12c2260432960c406fdd4873484
Fabrice Reix <fabrice.reix@orange.com>
2024-01-26 08:26:36 +0100

    Add hurlfmt integ test for standalone HTML

---
+++ integration/hurlfmt/tests_ok/html_standalone.ps1
@@ -0,0 +1,3 @@
+Set-StrictMode -Version latest
+$ErrorActionPreference = 'Stop'
+hurlfmt --out html --standalone tests_ok/html_standalone.hurl
---
+++ integration/hurlfmt/tests_ok/html_standalone.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+set -Eeuo pipefail
+hurlfmt --out html --standalone tests_ok/html_standalone.hurl
