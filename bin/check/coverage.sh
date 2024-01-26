e8f6e35a94a1acbb6b295da5552710163abcb346
Fabrice Reix <fabrice.reix@orange.com>
2024-01-26 11:58:42 +0100

    Check 100% coverage for HTML format

--- bin/check/coverage.sh
+++ bin/check/coverage.sh
@@ -4,7 +4,9 @@ bin/install_prerequisites_ubuntu.sh
 bin/install_grcov.sh
 bin/test/test_prerequisites.sh
 bin/coverage_run.sh
-lines=$(bin/coverage_uncovered_lines.py packages/hurlfmt/src/format/json.rs)
+lines=$(bin/coverage_uncovered_lines.py \
+  packages/hurl_core/src/format/html.rs \
+  packages/hurlfmt/src/format/json.rs)
 if [ -n "$lines" ]; then
     echo "$lines"
     exit 1
