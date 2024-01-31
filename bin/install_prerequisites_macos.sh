e44daea54b836f9558907c9d3d64e96fb374054f
Filipe PINTO <filipe.pinto@orange.com>
2024-01-31 13:22:50 +0100

    Add test-macos-arm64 job

--- bin/install_prerequisites_macos.sh
+++ bin/install_prerequisites_macos.sh
@@ -2,7 +2,7 @@
 set -Eeuo pipefail
 
 echo "----- install prerequisite packages -----"
-brew install curl pkg-config squid
+brew install bash pkg-config squid
 sudo squid -k shutdown || true
 sudo rm -v /dev/shm/squid*.shm >/dev/null 2>&1 || true
 
--- integration/hurl/test_curl_commands.sh
+++ integration/hurl/test_curl_commands.sh
@@ -1,15 +1,37 @@
 #!/bin/bash
 set -Eeuo pipefail
 
+color_red=$(echo -e "\033[1;31m")
+color_yellow=$(echo -ne "\033[1;33m")
+color_green=$(echo -ne "\033[1;32m")
+color_reset=$(echo -e "\033[0m")
+
+cmd_find_test_files="find ./tests_ok ./tests_failed -maxdepth 1 -type f -name '*.curl' ! -name '*windows*'"
+exclude_lines="^#"
+if ! curl --version | grep brotli ; then
+    exclude_lines="^#|brotli"
+    echo "${color_yellow}! Brotli tests excluded because curl does not contain this functionality in this system${color_reset}"
+fi
+
 while read -r test_file ; do
     echo "** ${test_file}"
+    cmd_find_lines="(grep -v '^$' \"${test_file}\" || true) | (grep -Ev \"$exclude_lines\" || true)"
     while read -r line;  do
         echo "${line}"
         cmd="${line} --no-progress-meter --output /dev/null --fail"
-        echo "${cmd}" | bash  || (echo ">>> Error <<<<" && exit 1)
-    done < <( (grep -v '^$' "${test_file}" || true) | (grep -v '^#' || true) )
+        eval "$cmd" && exit_code=0 || exit_code=1
+        if [[ $exit_code -eq 1 ]] ; then
+            echo "${color_red}>>> Error executing curl command${color_reset}"
+            echo "cmd=$cmd"
+            cmd="${cmd} --verbose"
+            result=$(bash -c "$cmd" || true)
+            echo "$result"
+            echo "${color_red}<<<${color_reset}"
+            exit 1
+        fi
+    done < <(eval "$cmd_find_lines")
     echo
-done < <( find ./tests_ok ./tests_failed -maxdepth 1 -type f -name '*.curl' ! -name '*windows*' | sort )
+done < <(eval "$cmd_find_test_files")
 
-echo "all curl commands have been run with success!"
+echo "${color_green}all curl commands have been run with success!${color_reset}"
 
