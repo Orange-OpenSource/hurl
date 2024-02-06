cfea0dc7cb4215d804ffce9ec1b1da0862e91bfd
Filipe PINTO <filipe.pinto@orange.com>
2024-02-06 15:55:59 +0100

    Force curl 8.5.0 on macos while waiting for curl 8.6.1

--- bin/install_prerequisites_macos.sh
+++ bin/install_prerequisites_macos.sh
@@ -2,6 +2,13 @@
 set -Eeuo pipefail
 
 echo "----- install prerequisite packages -----"
+### install curl 8.5.0 waiting for 8.6.1 (https://github.com/curl/curl/issues/12844)
+curl -o curl.rb https://raw.githubusercontent.com/Homebrew/homebrew-core/5f1b24e9882a1c1effa559a0000ff03ae155560b/Formula/c/curl.rb
+brew uninstall --force --ignore-dependencies curl
+brew install -s curl.rb
+PATH="/usr/local/opt/curl/bin/:$PATH"
+export PATH
+###
 brew install bash pkg-config squid
 sudo squid -k shutdown || true
 sudo rm -v /dev/shm/squid*.shm >/dev/null 2>&1 || true
