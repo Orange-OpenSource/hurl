6f75e3359bf2e7679464f90ae219f265a7c26c2e
Yufan Lou <2263580+louy2@users.noreply.github.com>
2024-02-08 23:45:08 +0800

    Fix tarball layout to Linux filesystem
    Fixes #2401

--- bin/release/create_tarball.sh
+++ bin/release/create_tarball.sh
@@ -10,10 +10,11 @@ echo "package_dir=${package_dir}"
 tarball_file="hurl-${package_signature}.tar.gz"
 echo "tarball_file=${tarball_file}"
 
-mkdir -p "${package_dir}"
-cp target/release/hurl "${package_dir}"
-cp target/release/hurlfmt "${package_dir}"
-cp target/man/* "${package_dir}"
+mkdir -p "${package_dir}/bin"
+cp target/release/hurl "${package_dir}/bin"
+cp target/release/hurlfmt "${package_dir}/bin"
+mkdir -p "${package_dir}/share/man/man1"
+cp target/man/* "${package_dir}/share/man/man1"
 
 mkdir -p target/upload
 tar cvfz "target/upload/${tarball_file}" -C "$(dirname "${package_dir}")" "hurl-${package_signature}"
