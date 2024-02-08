b543c4b52260e4d819bc1eaa260dade8a3910874
Fabrice Reix <fabrice.reix@orange.com>
2024-02-08 18:57:47 +0100

    Re-organize Generic deliverable to standard Linux filesystem

--- bin/release/create_tarball.sh
+++ bin/release/create_tarball.sh
@@ -11,9 +11,11 @@ tarball_file="hurl-${package_signature}.tar.gz"
 echo "tarball_file=${tarball_file}"
 
 mkdir -p "${package_dir}"
-cp target/release/hurl "${package_dir}"
-cp target/release/hurlfmt "${package_dir}"
-cp target/man/* "${package_dir}"
+mkdir -p "${package_dir}/bin"
+mkdir -p "${package_dir}/man/man1"
+
+cp target/release/{hurl,hurlfmt} "${package_dir}/bin"
+cp target/man/* "${package_dir}/man/man1"
 
 mkdir -p target/upload
 tar cvfz "target/upload/${tarball_file}" -C "$(dirname "${package_dir}")" "hurl-${package_signature}"
