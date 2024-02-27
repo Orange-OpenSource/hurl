06167f6afede5eb4f8ae49b2504a24c7745f1e02
fpinto <filipe.pinto@orange.com>
2024-02-27 16:49:18 +0100

    Include ps1 completion file in Windows packages

--- bin/release/create_windows64_zip_package.ps1
+++ bin/release/create_windows64_zip_package.ps1
@@ -11,7 +11,7 @@ $toolchain=((((rustup show active-toolchain) -Split " ")[0]) -Split "-",2)[1]
 
 # create windows64 zip package
 cd $PSScriptRoot\..\..\target\win-package
-Get-ChildItem -Path *.dll, *hurl.exe, *hurlfmt.exe, *.txt, ../../*.md | Compress-Archive -DestinationPath hurl-${hurl_package_version}-${toolchain}.zip
+Get-ChildItem -Path *.dll, *hurl.exe, *hurlfmt.exe, *.txt, ../../*.md, ../../completions/_hurl.ps1 | Compress-Archive -DestinationPath hurl-${hurl_package_version}-${toolchain}.zip
 Get-ChildItem .\*win64.zip
 
 cd $actual_dir
