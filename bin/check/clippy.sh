0f457bd03b5229ad2bc35509adb050ccce0d2eff
jcamiel <jeanchristophe.amiel@orange.com>
2024-03-04 15:10:06 +0100

    Move clippy lint rules to Cargo.toml

--- bin/check/clippy.sh
+++ bin/check/clippy.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 set -Eeuo pipefail
-# https://rust-lang.github.io/rust-clippy/master/index.html
-cargo clippy -- \
-  --deny warnings \
-  --deny clippy::empty_structs_with_brackets \
-  --deny clippy::manual_string_new \
-  --deny clippy::semicolon_if_nothing_returned
-
+cargo clippy
