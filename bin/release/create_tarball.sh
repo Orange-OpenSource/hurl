80809262d485cf3178e5bb7fc438265e5c6b81f3
Fabrice Reix <fabrice.reix@orange.com>
2024-03-11 11:27:31 +0100

    Add completion files in Linux/MacOS packages

--- bin/release/create_tarball.sh
+++ bin/release/create_tarball.sh
@@ -12,9 +12,11 @@ echo "tarball_file=${tarball_file}"
 
 mkdir -p "${package_dir}"
 mkdir -p "${package_dir}/bin"
+mkdir -p "${package_dir}/completions"
 mkdir -p "${package_dir}/man/man1"
 
 cp target/release/{hurl,hurlfmt} "${package_dir}/bin"
+cp completions/{_hurl,_hurlfmt,hurl.bash,hurlfmt.bash,hurl.fish,hurlfmt.fish} "${package_dir}/completions"
 cp target/man/* "${package_dir}/man/man1"
 
 mkdir -p target/upload
