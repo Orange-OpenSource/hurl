e387e8226fdadcd56c5aaf5bd688368280724249
fpinto <filipe.pinto@orange.com>
2024-04-02 13:26:35 +0200

    Add verbose to environment.sh

--- bin/environment.sh
+++ bin/environment.sh
@@ -1,9 +1,37 @@
 #!/bin/bash
 set -Eeuo pipefail
 
-uname -a
-python3 -V
-which curl
-curl --version
 date
+
+echo "# os"
+uname -a
+
+echo "# python3"
+if command -V python3 ; then
+    which python3
+    python3 -V 
+    which pip
+    pip --version
+else
+    echo "No python3 installed"
+fi
+
+echo "# curl"
+if command -V curl ; then
+    which curl || true
+    curl --version || true
+else
+    echo "No curl installed"
+fi
+
+
+echo  "# rust"
+if command -V rustc ; then
+    which rustc 
+    rustc --version
+    which cargo
+    cargo --version
+else
+    echo "No rust installed"
+fi
 
