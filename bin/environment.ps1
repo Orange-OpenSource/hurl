57850c8135b276d9bdd98947acb1e8b541d8b11b
fpinto <filipe.pinto@orange.com>
2024-04-02 13:30:51 +0200

    Add verbose to environment.ps1

--- bin/environment.ps1
+++ bin/environment.ps1
@@ -3,21 +3,34 @@ $ErrorActionPreference = 'Stop'
 
 write-host -foregroundcolor Cyan "----- context -----"
 
-# get windows infos
+write-host "# os"
 Get-ComputerInfo -Property WindowsProductName,WindowsVersion,OsHardwareAbstractionLayer
 
-# get pwsh infos
+write-host "# powershell"
 (Get-Variable PSVersionTable -ValueOnly).PSVersion
 
-# get vcpkg infos
+write-host "# vcpkg"
+(Get-Command -Name vcpkg -CommandType Application).Source
 vcpkg --version
 if ($LASTEXITCODE) { Throw }
 
-# get python infos
+write-host "# python"
+(Get-Command -Name python -CommandType Application).Source
 python -V
 if ($LASTEXITCODE) { Throw }
+(Get-Command -Name pip -CommandType Application).Source
+pip --version
+if ($LASTEXITCODE) { Throw }
 
-# get cargo info
+write-host "# curl"
+(Get-Command -Name curl -CommandType Application).Source
+curl --version
+if ($LASTEXITCODE) { Throw }
+
+write-host "# rust"
+(Get-Command -Name rustc -CommandType Application).Source
+rustc --version
+(Get-Command -Name cargo -CommandType Application).Source
 cargo --version
 if ($LASTEXITCODE) { Throw }
 
