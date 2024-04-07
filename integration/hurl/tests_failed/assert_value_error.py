0c2577f6f080d993a1a8513184522447e0c2585a
Fabrice Reix <fabrice.reix@orange.com>
2024-04-07 19:15:56 +0200

    Test CRLF in error message

--- integration/hurl/tests_failed/assert_value_error.py
+++ integration/hurl/tests_failed/assert_value_error.py
@@ -3,4 +3,4 @@ from app import app
 
 @app.route("/error-assert-value")
 def error_assert_value():
-    return '{ "values": [1,2,3], "count": 2}'
+    return '{ "values": [1,2,3], "count": 2, "line_terminator": "\\n"}'
