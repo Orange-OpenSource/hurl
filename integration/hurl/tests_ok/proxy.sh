3667ccd4cd32f0fe49fa8579fa78eb35280ea59a
jcamiel <jeanchristophe.amiel@orange.com>
2024-04-11 17:01:47 +0200

    Fix integration test format.

--- integration/hurl/tests_ok/proxy.sh
+++ integration/hurl/tests_ok/proxy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -Eeuo pipefail
-hurl tests_ok/proxy.hurl --proxy localhost:3128 --verbose
-hurl tests_ok/proxy.hurl --proxy 127.0.0.1:3128 --verbose
+hurl --proxy localhost:3128 --verbose tests_ok/proxy.hurl
+hurl --proxy 127.0.0.1:3128 --verbose tests_ok/proxy.hurl
 
