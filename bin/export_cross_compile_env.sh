cd0bac577ce1d5a35d34e623c73ccfd554dc5ed6
fpinto <filipe.pinto@orange.com>
2024-04-25 17:04:39 +0200

    Add generic linux aarch64 package to extra-packages

--- bin/export_cross_compile_env.sh
+++ bin/export_cross_compile_env.sh
@@ -5,5 +5,8 @@ PKG_CONFIG_ALLOW_CROSS=1
 CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_LINKER=aarch64-linux-gnu-gcc
 CC_aarch64_unknown_linux_gnu=aarch64-linux-gnu-gcc
 CXX_aarch64_unknown_linux_gnu=aarch64-linux-gnu-g++
-export PKG_CONFIG_ALLOW_CROSS CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_LINKER CC_aarch64_unknown_linux_gnu CXX_aarch64_unknown_linux_gnu
+export PKG_CONFIG_ALLOW_CROSS \
+    CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_LINKER \
+    CC_aarch64_unknown_linux_gnu \
+    CXX_aarch64_unknown_linux_gnu
 
