f01287c9014f23c4a4210078468f3bc868431dae
Jean-Christophe Amiel <jeanchristophe.amiel@orange.com>
2024-04-30 11:43:54 +0200

    Patch Arch Linux prerequistes (temporary).

--- bin/install_prerequisites_docker_archlinux.sh
+++ bin/install_prerequisites_docker_archlinux.sh
@@ -3,6 +3,8 @@ set -Eeuo pipefail
 
 echo "----- install prerequisite packages -----"
 pacman -Sy --noconfirm bash sudo python3 python-pip icu base-devel libxml2 glibc openbsd-netcat squid jq
+# Temporary install to patch a python3/pip crash
+pacman -Sy --noconfirm expat
 sudo squid -k shutdown || true
 sudo rm -v /dev/shm/squid*.shm >/dev/null 2>&1 || true
 
