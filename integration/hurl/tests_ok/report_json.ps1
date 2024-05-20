c3d0cecbc9dcfbbe3f6296f2d95290b364527517
Jean-Christophe Amiel <jeanchristophe.amiel@orange.com>
2024-05-20 13:36:43 +0200

    Add integration test for --report-json option.

---
+++ integration/hurl/tests_ok/report_json.ps1
@@ -0,0 +1,13 @@
+Set-StrictMode -Version latest
+$ErrorActionPreference = 'Stop'
+if (Test-Path build/report/json) {
+    Remove-Item -Recurse build/report/json
+}
+
+# test.2.hurl is KO but we want the script to continue until the end
+$ErrorActionPreference = 'Continue'
+hurl --test --report-json build/report/json tests_ok/test.1.hurl tests_ok/test.2.hurl
+hurl --test --report-json build/report/json tests_ok/test.3.hurl
+$ErrorActionPreference = 'Stop'
+
+Write-Host (Get-Content build/report/json/index.json -Raw) -NoNewLine
