1831f4eb4d7be8a28913acc16a8725e39e0d97e9
Fabrice Reix <fabrice.reix@orange.com>
2024-06-01 09:46:46 +0200

    Add append and split methods to StyledString

--- packages/hurl_core/src/text/styledstring.rs
+++ packages/hurl_core/src/text/styledstring.rs
@@ -64,6 +64,35 @@ impl<'a> StyledString<'a> {
             .collect::<Vec<String>>()
             .join("")
     }
+
+    pub fn append(&mut self, other: StyledString<'a>) {
+        for token in other.tokens {
+            self.tokens.push(token);
+        }
+    }
+
+    pub fn split(&self, delimiter: char) -> Vec<StyledString> {
+        let mut items = vec![];
+        let mut item = StyledString::new();
+        for token in &self.tokens {
+            let mut substrings = token.content.split(delimiter).collect::<Vec<&str>>();
+            let first = substrings.remove(0);
+            if !first.is_empty() {
+                item.push_with(first, token.style.clone());
+            }
+            for substring in substrings {
+                items.push(item);
+                item = StyledString::new();
+                if !substring.is_empty() {
+                    item.push_with(substring, token.style.clone());
+                }
+            }
+        }
+
+        items.push(item);
+
+        items
+    }
 }
 
 impl<'a> Token<'a> {
@@ -152,6 +181,62 @@ mod tests {
             message.to_string(Format::Ansi),
             "Hello \u{1b}[31mBob\u{1b}[0m!"
         );
+    }
+
+    #[test]
+    fn test_append() {
+        let mut message1 = StyledString::new();
+        message1.push("Hello ");
+        message1.push_with("Bob", Style::new().red());
+        message1.push("!");
+        let mut message2 = StyledString::new();
+        message2.push("Hi ");
+        message2.push_with("Bill", Style::new().red());
+        message2.push("!");
+
+        let mut messages = StyledString::new();
+        messages.push("Hello ");
+        messages.push_with("Bob", Style::new().red());
+        messages.push("!");
+        messages.push("Hi ");
+        messages.push_with("Bill", Style::new().red());
+        messages.push("!");
+
+        message1.append(message2);
+        assert_eq!(message1, messages);
+    }
+
+    #[test]
+    fn test_split() {
+        let mut line = StyledString::new();
+        line.push("Hello,Hi,");
+        line.push_with("Hola", Style::new().red());
+        line.push(",Bye,");
+        line.push_with("Adios", Style::new().red());
+
+        let mut item0 = StyledString::new();
+        item0.push("Hello");
+        let mut item1 = StyledString::new();
+        item1.push("Hi");
+        let mut item2 = StyledString::new();
+        item2.push_with("Hola", Style::new().red());
+        let mut item3 = StyledString::new();
+        item3.push("Bye");
+        let mut item4 = StyledString::new();
+        item4.push_with("Adios", Style::new().red());
+        assert_eq!(line.split(','), vec![item0, item1, item2, item3, item4]);
+
+        // Test empty items
+        let mut line = StyledString::new();
+        line.push("0,,2,");
+
+        let mut item0 = StyledString::new();
+        item0.push("0");
+        let item1 = StyledString::new();
+        let mut item2 = StyledString::new();
+        item2.push("2");
+        let item3 = StyledString::new();
+        assert_eq!(line.split(','), vec![item0, item1, item2, item3]);
     }
 
     #[test]
