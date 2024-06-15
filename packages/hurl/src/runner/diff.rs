33a8b67a9d0c656850c730a41af2c1e738d605d9
Fabrice Reix <fabrice.reix@orange.com>
2024-06-15 08:32:39 +0200

    Update Diff API with hunks

--- packages/hurl/src/runner/diff.rs
+++ packages/hurl/src/runner/diff.rs
@@ -17,15 +17,24 @@
  */
 
 use hurl_core::text::{Style, StyledString};
-use similar::{ChangeTag, TextDiff};
+use similar::{ChangeTag, DiffOp, DiffTag, TextDiff};
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct DiffHunk {
+    content: StyledString,
+    start: usize,       // 0-based
+    source_line: usize, // 0-based
+}
 
 #[allow(dead_code)]
-pub fn diff(expected: &str, actual: &str) -> StyledString {
-    let text_diff = TextDiff::from_lines(expected, actual);
+pub fn diff(old: &str, new: &str) -> Vec<DiffHunk> {
+    let text_diff = TextDiff::from_lines(old, new);
     let unified_diff = text_diff.unified_diff();
 
-    let mut s = StyledString::new();
+    let mut hunks = vec![];
     for hunk in unified_diff.iter_hunks() {
+        let (start, source_line) = get_hunk_lines(hunk.ops());
+        let mut content = StyledString::new();
         for change in hunk.iter_changes() {
             let sign = match change.tag() {
                 ChangeTag::Delete => "-",
@@ -38,20 +47,42 @@ pub fn diff(expected: &str, actual: &str) -> StyledString {
                 ChangeTag::Insert => Style::new().green(),
                 ChangeTag::Equal => Style::new(),
             };
-            s.push_with(&line, style);
+            content.push_with(&line, style);
+        }
+        let hunk = DiffHunk {
+            content,
+            source_line,
+            start,
+        };
+        hunks.push(hunk);
+    }
+    hunks
+}
+
+/// get start of the hunk and first change in the input source string
+/// Both are 0-based line number
+fn get_hunk_lines(ops: &[DiffOp]) -> (usize, usize) {
+    let mut start = 0;
+    for op in ops {
+        match op.tag() {
+            DiffTag::Equal => {
+                start = op.old_range().start;
+            }
+            DiffTag::Delete => return (start, op.old_range().start),
+            DiffTag::Insert => return (start, op.old_range().start - 1),
+            DiffTag::Replace => return (start, op.old_range().start),
         }
     }
-    s
+    (start, 0)
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use hurl_core::text::{Format, Style, StyledString};
+    use hurl_core::text::{Style, StyledString};
 
-    #[test]
-    fn test_diff_json_strings() {
-        let old = r#"{
+    fn old_string() -> String {
+        r#"{
   "first_name": "John",
   "last_name": "Smith",
   "is_alive": true,
@@ -79,9 +110,111 @@ mod tests {
   ],
   "spouse": null
 }
-"#;
+"#
+        .to_string()
+    }
 
-        let new = r#"{
+    fn new_string_change_line1() -> String {
+        r#"[
+  "first_name": "John",
+  "last_name": "Smith",
+  "is_alive": true,
+  "age": 27,
+  "address": {
+    "street_address": "21 2nd Street",
+    "city": "New York",
+    "state": "NY",
+    "postal_code": "10021-3100"
+  },
+  "phone_numbers": [
+    {
+      "type": "home",
+      "number": "212 555-1234"
+    },
+    {
+      "type": "office",
+      "number": "646 555-4567"
+    }
+  ],
+  "children": [
+    "Catherine",
+    "Thomas",
+    "Trevor"
+  ],
+  "spouse": null
+}
+"#
+        .to_string()
+    }
+
+    fn new_string_change_line2() -> String {
+        r#"{
+  "first_name": "Bob",
+  "last_name": "Smith",
+  "is_alive": true,
+  "age": 27,
+  "address": {
+    "street_address": "21 2nd Street",
+    "city": "New York",
+    "state": "NY",
+    "postal_code": "10021-3100"
+  },
+  "phone_numbers": [
+    {
+      "type": "home",
+      "number": "212 555-1234"
+    },
+    {
+      "type": "office",
+      "number": "646 555-4567"
+    }
+  ],
+  "children": [
+    "Catherine",
+    "Thomas",
+    "Trevor"
+  ],
+  "spouse": null
+}
+"#
+        .to_string()
+    }
+
+    fn new_string_change_line3() -> String {
+        r#"{
+  "first_name": "John",
+  "last_name": "Smiths",
+  "is_alive": true,
+  "age": 27,
+  "address": {
+    "street_address": "21 2nd Street",
+    "city": "New York",
+    "state": "NY",
+    "postal_code": "10021-3100"
+  },
+  "phone_numbers": [
+    {
+      "type": "home",
+      "number": "212 555-1234"
+    },
+    {
+      "type": "office",
+      "number": "646 555-4567"
+    }
+  ],
+  "children": [
+    "Catherine",
+    "Thomas",
+    "Trevor"
+  ],
+  "spouse": null
+}
+"#
+        .to_string()
+    }
+
+    fn new_string_change_line4() -> String {
+        r#"{
   "first_name": "John",
   "last_name": "Smith",
   "is_alive": true,
@@ -109,47 +242,346 @@ mod tests {
   ],
   "spouse": null
 }
-"#;
+"#
+        .to_string()
+    }
 
-        let mut diff_output = StyledString::new();
-        diff_output.push(
+    fn new_string_delete_line3() -> String {
+        r#"{
+  "first_name": "John",
+  "is_alive": true,
+  "age": 27,
+  "address": {
+    "street_address": "21 2nd Street",
+    "city": "New York",
+    "state": "NY",
+    "postal_code": "10021-3100"
+  },
+  "phone_numbers": [
+    {
+      "type": "home",
+      "number": "212 555-1234"
+    },
+    {
+      "type": "office",
+      "number": "646 555-4567"
+    }
+  ],
+  "children": [
+    "Catherine",
+    "Thomas",
+    "Trevor"
+  ],
+  "spouse": null
+}
+"#
+        .to_string()
+    }
+
+    fn new_string_add_line3() -> String {
+        r#"{
+  "first_name": "John",
+  "middle_name": "Bob",
+  "last_name": "Smith",
+  "is_alive": true,
+  "age": 27,
+  "address": {
+    "street_address": "21 2nd Street",
+    "city": "New York",
+    "state": "NY",
+    "postal_code": "10021-3100"
+  },
+  "phone_numbers": [
+    {
+      "type": "home",
+      "number": "212 555-1234"
+    },
+    {
+      "type": "office",
+      "number": "646 555-4567"
+    }
+  ],
+  "children": [
+    "Catherine",
+    "Thomas",
+    "Trevor"
+  ],
+  "spouse": null
+}
+"#
+        .to_string()
+    }
+
+    fn new_string_change_line4_line24() -> String {
+        r#"{
+  "first_name": "John",
+  "last_name": "Smith",
+  "is_alive": true,
+  "age": 28,
+  "address": {
+    "street_address": "21 2nd Street",
+    "city": "New York",
+    "state": "NY",
+    "postal_code": "10021-3100"
+  },
+  "phone_numbers": [
+    {
+      "type": "home",
+      "number": "212 555-1234"
+    },
+    {
+      "type": "office",
+      "number": "646 555-4567"
+    }
+  ],
+  "children": [
+    "Catherine",
+    "Thomas",
+    "Bob"
+  ],
+  "spouse": null
+}
+"#
+        .to_string()
+    }
+
+    #[test]
+    fn test_diff_change_line1() {
+        let mut expected_diff_output = StyledString::new();
+        expected_diff_output.push_with(
+            r#"-{
+"#,
+            Style::new().red(),
+        );
+        expected_diff_output.push_with(
+            r#"+[
+"#,
+            Style::new().green(),
+        );
+        expected_diff_output.push(
             r#"   "first_name": "John",
    "last_name": "Smith",
    "is_alive": true,
 "#,
         );
-        diff_output.push_with(
+
+        let hunks = diff(&old_string(), &new_string_change_line1());
+        let first_hunk = hunks.first().unwrap().clone();
+        assert_eq!(first_hunk.content, expected_diff_output);
+        assert_eq!(first_hunk.start, 0);
+        assert_eq!(first_hunk.source_line, 0);
+    }
+
+    #[test]
+    fn test_diff_change_line2() {
+        let mut expected_diff_output = StyledString::new();
+        expected_diff_output.push(
+            r#" {
+"#,
+        );
+        expected_diff_output.push_with(
+            r#"-  "first_name": "John",
+"#,
+            Style::new().red(),
+        );
+        expected_diff_output.push_with(
+            r#"+  "first_name": "Bob",
+"#,
+            Style::new().green(),
+        );
+        expected_diff_output.push(
+            r#"   "last_name": "Smith",
+   "is_alive": true,
+   "age": 27,
+"#,
+        );
+
+        let hunks = diff(&old_string(), &new_string_change_line2());
+        let first_hunk = hunks.first().unwrap().clone();
+        assert_eq!(first_hunk.content, expected_diff_output);
+        assert_eq!(first_hunk.start, 0);
+        assert_eq!(first_hunk.source_line, 1);
+    }
+
+    #[test]
+    fn test_diff_change_line3() {
+        let mut expected_diff_output = StyledString::new();
+        expected_diff_output.push(
+            r#" {
+   "first_name": "John",
+"#,
+        );
+        expected_diff_output.push_with(
+            r#"-  "last_name": "Smith",
+"#,
+            Style::new().red(),
+        );
+        expected_diff_output.push_with(
+            r#"+  "last_name": "Smiths",
+"#,
+            Style::new().green(),
+        );
+        expected_diff_output.push(
+            r#"   "is_alive": true,
+   "age": 27,
+   "address": {
+"#,
+        );
+
+        let hunks = diff(&old_string(), &new_string_change_line3());
+        let first_hunk = hunks.first().unwrap().clone();
+        assert_eq!(first_hunk.content, expected_diff_output);
+        assert_eq!(first_hunk.start, 0);
+        assert_eq!(first_hunk.source_line, 2);
+    }
+
+    #[test]
+    fn test_diff_change_line4() {
+        let mut expected_diff_output = StyledString::new();
+        expected_diff_output.push(
+            r#"   "first_name": "John",
+   "last_name": "Smith",
+   "is_alive": true,
+"#,
+        );
+        expected_diff_output.push_with(
             r#"-  "age": 27,
 "#,
             Style::new().red(),
         );
-        diff_output.push_with(
+        expected_diff_output.push_with(
             r#"+  "age": 28,
 "#,
             Style::new().green(),
         );
-        diff_output.push(
+        expected_diff_output.push(
             r#"   "address": {
      "street_address": "21 2nd Street",
      "city": "New York",
 "#,
         );
+        let hunks = diff(&old_string(), &new_string_change_line4());
 
-        assert_eq!(diff(old, new), diff_output);
+        let first_hunk = hunks.first().unwrap().clone();
+        assert_eq!(first_hunk.content, expected_diff_output);
+        assert_eq!(first_hunk.start, 1);
+        assert_eq!(first_hunk.source_line, 4);
+    }
 
-        let diff_output_plain = r#"   "first_name": "John",
+    #[test]
+    fn test_diff_delete_line3() {
+        let mut expected_diff_output = StyledString::new();
+        expected_diff_output.push(
+            r#" {
+   "first_name": "John",
+"#,
+        );
+        expected_diff_output.push_with(
+            r#"-  "last_name": "Smith",
+"#,
+            Style::new().red(),
+        );
+        expected_diff_output.push(
+            r#"   "is_alive": true,
+   "age": 27,
+   "address": {
+"#,
+        );
+
+        let hunks = diff(&old_string(), &new_string_delete_line3());
+        let first_hunk = hunks.first().unwrap().clone();
+        assert_eq!(first_hunk.content, expected_diff_output);
+        assert_eq!(first_hunk.start, 0);
+        assert_eq!(first_hunk.source_line, 2);
+    }
+
+    #[test]
+    fn test_diff_add_line3() {
+        let mut expected_diff_output = StyledString::new();
+        expected_diff_output.push(
+            r#" {
+   "first_name": "John",
+"#,
+        );
+
+        expected_diff_output.push_with(
+            r#"+  "middle_name": "Bob",
+"#,
+            Style::new().green(),
+        );
+        expected_diff_output.push(
+            r#"   "last_name": "Smith",
+   "is_alive": true,
+   "age": 27,
+"#,
+        );
+
+        let hunks = diff(&old_string(), &new_string_add_line3());
+        let first_hunk = hunks.first().unwrap().clone();
+        assert_eq!(first_hunk.content, expected_diff_output);
+        assert_eq!(first_hunk.start, 0);
+        assert_eq!(first_hunk.source_line, 1);
+    }
+
+    #[test]
+    fn test_diff_change_line4_line24() {
+        let hunks = diff(&old_string(), &new_string_change_line4_line24());
+
+        let first_hunk = hunks.first().unwrap().clone();
+        let mut expected_diff_output = StyledString::new();
+        expected_diff_output.push(
+            r#"   "first_name": "John",
    "last_name": "Smith",
    "is_alive": true,
--  "age": 27,
-+  "age": 28,
-   "address": {
+"#,
+        );
+        expected_diff_output.push_with(
+            r#"-  "age": 27,
+"#,
+            Style::new().red(),
+        );
+        expected_diff_output.push_with(
+            r#"+  "age": 28,
+"#,
+            Style::new().green(),
+        );
+        expected_diff_output.push(
+            r#"   "address": {
      "street_address": "21 2nd Street",
      "city": "New York",
-"#;
-        assert_eq!(diff(old, new).to_string(Format::Plain), diff_output_plain);
+"#,
+        );
+        assert_eq!(first_hunk.content, expected_diff_output);
+        assert_eq!(first_hunk.start, 1);
+        assert_eq!(first_hunk.source_line, 4);
 
-        colored::control::set_override(true);
-        let diff_output_colored = "   \"first_name\": \"John\",\n   \"last_name\": \"Smith\",\n   \"is_alive\": true,\n\u{1b}[31m-  \"age\": 27,\n\u{1b}[0m\u{1b}[32m+  \"age\": 28,\n\u{1b}[0m   \"address\": {\n     \"street_address\": \"21 2nd Street\",\n     \"city\": \"New York\",\n";
-        assert_eq!(diff(old, new).to_string(Format::Ansi), diff_output_colored);
+        let second_hunk = hunks.get(1).unwrap().clone();
+        let mut expected_diff_output = StyledString::new();
+        expected_diff_output.push(
+            r#"   "children": [
+     "Catherine",
+     "Thomas",
+"#,
+        );
+        expected_diff_output.push_with(
+            r#"-    "Trevor"
+"#,
+            Style::new().red(),
+        );
+        expected_diff_output.push_with(
+            r#"+    "Bob"
+"#,
+            Style::new().green(),
+        );
+        expected_diff_output.push(
+            r#"   ],
+   "spouse": null
+ }
+"#,
+        );
+
+        assert_eq!(second_hunk.content, expected_diff_output);
+        assert_eq!(second_hunk.start, 21);
+        assert_eq!(second_hunk.source_line, 24);
     }
 }
