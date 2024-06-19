1c66b2e6ccfafb5c3edd22f33607ed40f0641232
Filipe PINTO <lepapareil@gmail.com>
2024-06-19 10:42:06 +0200

    Fix Archlinux ssl issue when downloading python deps

--- bin/install_prerequisites_archlinux.sh
+++ bin/install_prerequisites_archlinux.sh
@@ -2,8 +2,9 @@
 set -Eeuo pipefail
 
 echo "----- install prerequisite packages -----"
-pacman -Syu --noconfirm bash sudo icu base-devel libxml2 python3 python3-venv glibc openbsd-netcat squid jq
+pacman -Sy --noconfirm bash sudo openssl python3 python-pip icu base-devel libxml2 glibc openbsd-netcat squid jq
 # Temporary install to patch a python3/pip crash
-pacman -Syu --noconfirm expat
+pacman -Sy --noconfirm expat
 sudo squid -k shutdown || true
 sudo rm -v /dev/shm/squid*.shm >/dev/null 2>&1 || true
+
