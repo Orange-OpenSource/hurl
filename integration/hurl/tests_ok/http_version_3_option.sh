958722f750a711f4bee57f38d4f4293703d3c91d
Jean-Christophe Amiel <jeanchristophe.amiel@orange.com>
2024-07-17 18:14:21 +0200

    Desactivate KO integration test on Arch Linux.

--- integration/hurl/tests_ok/http_version_3_option.sh
+++ integration/hurl/tests_ok/http_version_3_option.sh
@@ -6,6 +6,14 @@ curl --version | grep Features | grep -q HTTP3
 if [ $? -eq 1 ]; then
   exit 255
 fi
+
+# On désactive ce test momentanément Sur Arch Linux. Arch Linux supporte
+# maintenant HTTP/3 mais ce test ne passe pas.
+if [ -f /etc/os-release ]; then
+    if grep -q 'NAME="Arch Linux"' /etc/os-release; then
+        exit 255
+    fi
+fi
 set -Eeuo pipefail
 
 hurl tests_ok/http_version_3_option.hurl
