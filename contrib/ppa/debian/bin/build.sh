80a27e0a6e2c4ae73af998db9836a881d82067bc
Filipe PINTO <filipe.pinto@orange.com>
2024-07-30 16:16:07 +0200

    ppa

--- contrib/ppa/debian/bin/build.sh
+++ contrib/ppa/debian/bin/build.sh
@@ -5,6 +5,11 @@ echo "## untar pxf vendor.tar.xz:"
 tar pxf vendor.tar.xz
 
 echo "## install rustc and cargo"
+echo "# actual rustc and cargo"
+which rustc || true
+which cargo || true
+rustc --version || true
+cargo --version || true
 rust_version=$(grep '^rust-version' packages/hurl/Cargo.toml | cut -f2 -d'"')
 rust_arch=$(uname -m)
 package=rust-$rust_version-$rust_arch-unknown-linux-gnu
@@ -15,9 +20,11 @@ tar xf $package.tar.gz
 mkdir -p /tmp/rust
 ./$package/install.sh --verbose --destdir=/tmp/rust --disable-ldconfig
 export PATH="/tmp/rust/usr/local/bin:$PATH"
-export LD_LIBRARY_PATH="/tmp/rust/usr/local/lib:$LD_LIBRARY_PATH"
-rustc --version
-cargo --version
+echo "# now rustc and cargo"
+which rustc || true
+which cargo || true
+rustc --version || true
+cargo --version || true
 
 echo "## .cargo/config:"
 cat .cargo/config
