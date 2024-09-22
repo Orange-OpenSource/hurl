4c48d57c8b5657122173f8851c166184ef042b77
Fabrice Reix <fabrice.reix@orange.com>
2024-09-22 21:38:47 +0200

    Add test integ for jsonpath bool filter

--- integration/hurl/tests_ok/jsonpath_store.py
+++ integration/hurl/tests_ok/jsonpath_store.py
@@ -9,22 +9,26 @@ def json_store():
   "store": {
     "book": [
       { "category": "reference",
+        "published": false,
         "author": "Nigel Rees",
         "title": "Sayings of the Century",
         "price": 8.95
       },
       { "category": "fiction",
+        "published": false,
         "author": "Evelyn Waugh",
         "title": "Sword of Honour",
         "price": 12.99
       },
       { "category": "fiction",
+        "published": true,
         "author": "Herman Melville",
         "title": "Moby Dick",
         "isbn": "0-553-21311-3",
         "price": 8.99
       },
       { "category": "fiction",
+        "published": false,
         "author": "J. R. R. Tolkien",
         "title": "The Lord of the Rings",
         "isbn": "0-395-19395-8",
