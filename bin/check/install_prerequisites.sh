deeaf020c0ba0a3e8e455a76ccb3298f97da48d2
fpinto <filipe.pinto@orange.com>
2024-10-15 20:37:20 +0200

    Fix python venv in checks ci

--- bin/check/install_prerequisites.sh
+++ bin/check/install_prerequisites.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -Eeuo pipefail
 
-bin/install_rust.sh
 python3 -m pip install --requirement bin/requirements-frozen.txt
-sudo apt-get update && sudo apt-get install -y libxml2-utils
+sudo apt-get update
+sudo apt-get install -y libxml2-utils
+bin/install_rust.sh
 
