77731b6ed4ae1e86565a693c43e2b87dd94c5f07
Filipe PINTO <filipe.pinto@orange.com>
2024-11-21 11:36:00 +0100

    Use system curl in CI/CD for macOS (ARM & x86)

--- bin/install_prerequisites_macos.sh
+++ bin/install_prerequisites_macos.sh
@@ -1,21 +1,9 @@
 #!/bin/bash
 set -Eeuo pipefail
 
-echo "----- preinstalled curl version -----"
-curl --version
-
 echo "----- install prerequisite packages -----"
 export HOMEBREW_NO_INSTALLED_DEPENDENTS_CHECK=true
-brew uninstall --force --ignore-dependencies curl
 brew update
-brew install -s curl
-brew link --force --overwrite curl
-CURL_PATH="$(brew --prefix curl)/bin"
-export CURL_PATH
-echo "CURL_PATH=$CURL_PATH"
-PATH="$CURL_PATH:$PATH"
-export PATH
 brew install bash expect pkg-config squid jq
 sudo squid -k shutdown || true
 sudo rm -v /dev/shm/squid*.shm >/dev/null 2>&1 || true
-
