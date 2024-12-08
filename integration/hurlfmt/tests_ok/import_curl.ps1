260a990f9c198b556ee72ef060bf081c331c834a
Jean-Christophe Amiel <jeanchristophe.amiel@orange.com>
2024-12-08 13:35:36 +0100

    Remove expected validation.

--- integration/hurlfmt/tests_ok/import_curl.ps1
+++ integration/hurlfmt/tests_ok/import_curl.ps1
@@ -1,4 +1,4 @@
 Set-StrictMode -Version latest
 $ErrorActionPreference = 'Stop'
-hurl --file-root . tests_ok/import_curl.out >$null  # Validate expected file
+
 hurlfmt --in curl tests_ok/import_curl.in
--- integration/hurlfmt/tests_ok/import_curl.sh
+++ integration/hurlfmt/tests_ok/import_curl.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -Eeuo pipefail
-hurl --file-root . tests_ok/import_curl.out >/dev/null  # Validate expected file
+
 hurlfmt --in curl tests_ok/import_curl.in
