62275934dcc2020672d4f40e55a20f90d49856b1
Filipe PINTO <filipe.pinto@orange.com>
2025-02-05 15:41:50 +0100

    Beautify tests duration logs

--- integration/test_script.py
+++ integration/test_script.py
@@ -32,11 +32,10 @@ def test(script_file: str):
         cmd = ["pwsh", "-Command", script_file, ";", "exit $LASTEXITCODE"]
     else:
         cmd = [script_file]
-    print(" ".join(cmd))
     start_time = time.time()
     result = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    execution_time = time.time() - start_time
-    print(f" - Duration: {execution_time} seconds")
+    execution_time = round(time.time() - start_time, 2)
+    print(f"{' '.join(cmd)} (Duration: {execution_time} seconds)")
 
     basename = os.path.splitext(script_file)[0]
 
