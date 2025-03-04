efc160efb12cb53040ec7c3da0dd1d0dd455261e
Filipe PINTO <filipe.pinto@orange.com>
2025-03-04 17:11:26 +0100

    Fix get_active_toolchain.sh

--- bin/release/get_active_toolchain.sh
+++ bin/release/get_active_toolchain.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -Eeuo pipefail
 
-toolchain=$(rustup show active-toolchain | cut -d '-' -f 2- | cut -d ' ' -f1)
+toolchain=$(rustup show active-toolchain | cut -d '-' -f 2- | cut -d ' ' -f1 | head -1)
 echo "${toolchain}"
 
