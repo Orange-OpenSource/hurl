6497604a5e5f66b94e034878051d3e722e8764da
Jean-Christophe Amiel <jeanchristophe.amiel@orange.com>
2025-04-04 10:15:05 +0200

    Relax test on proxy error message regression with curl 8.13.0
    See <https://github.com/curl/curl/issues/16958>

--- integration/hurl/tests_failed/proxy_host.ps1
+++ integration/hurl/tests_failed/proxy_host.ps1
@@ -1,3 +1,4 @@
 Set-StrictMode -Version latest
 $ErrorActionPreference = 'Stop'
+
 hurl --proxy unknown tests_ok/hello.hurl
--- integration/hurl/tests_failed/proxy_host.sh
+++ integration/hurl/tests_failed/proxy_host.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 set -Eeuo pipefail
+
 hurl --proxy unknown tests_ok/hello.hurl
