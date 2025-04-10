5034e510f157ca624fca16126297a267b4093e8a
Fabrice Reix <fabrice.reix@orange.com>
2025-04-10 15:15:54 +0200

    Display version for valgrind/cargo-valgrind

--- bin/check/valgrind.sh
+++ bin/check/valgrind.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -Eeuo pipefail
 
+valgrind --version
+cargo-valgrind --help
+
 cat <<END | cargo valgrind run -p hurl -- --test
 GET https://unpkg.com/vue@3.4.27/dist/vue.global.prod.js
 HTTP 200
