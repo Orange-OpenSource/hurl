b94a486e4cb938c72b2b7232c8b3727cb3396cce
Jean-Christophe Amiel <jeanchristophe.amiel@orange.com>
2025-05-16 08:50:24 +0200

    Move content_type integration tests in subfolder.

--- integration/hurl/tests_ok/content_type/content_type.ps1
+++ integration/hurl/tests_ok/content_type/content_type.ps1
@@ -1,3 +1,4 @@
 Set-StrictMode -Version latest
 $ErrorActionPreference = 'Stop'
-hurl --verbose tests_ok/content_type.hurl
+
+hurl --verbose tests_ok/content_type/content_type.hurl
---
+++ integration/hurl/tests_ok/content_type/content_type.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -Eeuo pipefail
+
+hurl --verbose tests_ok/content_type/content_type.hurl
+
