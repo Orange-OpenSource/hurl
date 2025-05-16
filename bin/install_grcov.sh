092c1c9dec247037a074f66d29f46243239a76d9
Jean-Christophe Amiel <jeanchristophe.amiel@orange.com>
2025-05-16 08:35:56 +0200

    Update to latest grcov

--- bin/install_grcov.sh
+++ bin/install_grcov.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 set -Eeuo pipefail
 
-# See <https://github.com/mozilla/grcov/issues/1351>
-# grcov v0.9.1 installation fails
-cargo install grcov --version 0.8.20
+cargo install grcov
 rustup component add llvm-tools-preview
 cargo clean
 
