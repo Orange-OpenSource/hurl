c46b2c4aa6d2209201c5299f91ee202493888f10
Jean-Christophe Amiel <jeanchristophe.amiel@orange.com>
2025-05-17 13:32:08 +0200

    Move fileroot integration tests in subfolder.

--- integration/hurl/tests_ok/fileroot.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -Eeuo pipefail
-rm -f build/fileroot.bin
-hurl --file-root build/ tests_ok/fileroot.hurl
--- integration/hurl/tests_ok/fileroot/fileroot.ps1
+++ integration/hurl/tests_ok/fileroot/fileroot.ps1
@@ -1,6 +1,7 @@
 Set-StrictMode -Version latest
 $ErrorActionPreference = 'Stop'
+
 if (Test-Path build/fileroot.bin) {
     Remove-Item build/fileroot.bin
 }
-hurl --file-root build/ tests_ok/fileroot.hurl
+hurl --file-root build/ tests_ok/fileroot/fileroot.hurl
