af8856c852a20be2fcc22cbb7edf24274e39e083
Jean-Christophe Amiel <jeanchristophe.amiel@orange.com>
2025-05-19 16:46:15 +0200

    Move help integration tests in subfolder.

--- integration/hurl/tests_ok/help/help.ps1
+++ integration/hurl/tests_ok/help/help.ps1
@@ -1,3 +1,4 @@
 Set-StrictMode -Version latest
 $ErrorActionPreference = 'Stop'
+
 hurl --help
--- integration/hurl/tests_ok/help/help.sh
+++ integration/hurl/tests_ok/help/help.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -Eeuo pipefail
+
 # In CI, --help is wrapped on a 100 columns wide terminal.
 hurl --help
