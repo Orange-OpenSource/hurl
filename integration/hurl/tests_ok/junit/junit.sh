2bcffb5388c6873e8d5027df1b65ea862173305f
Jean-Christophe Amiel <jeanchristophe.amiel@orange.com>
2025-05-20 13:53:07 +0200

    Move junit integration tests in subfolder.

---
+++ integration/hurl/tests_ok/junit/junit.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -Eeuo pipefail
+rm -f build/junit/result.xml
+
+# test2 is KO but we want the script to continue until the end
+set +eo pipefail
+# We use --jobs 1 to force the standard error order to be test1 then test2.
+hurl --test --jobs 1 --report-junit build/junit/result.xml tests_ok/junit/test.1.hurl tests_ok/junit/test.2.hurl
+hurl --test --report-junit build/junit/result.xml tests_ok/junit/test.3.hurl
+hurl --test --report-junit build/junit/result.xml tests_ok/junit/test.4.hurl
+set -Eeuo pipefail
+
+cat build/junit/result.xml
