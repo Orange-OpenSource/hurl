a86b4e4ec8fe35ffc46da094d100cc42fb4ff383
Jean-Christophe Amiel <jeanchristophe.amiel@orange.com>
2025-05-20 17:41:30 +0200

    Move tap integration tests in subfolder.

--- integration/hurl/tests_ok/tap/tap.ps1
+++ integration/hurl/tests_ok/tap/tap.ps1
@@ -6,8 +6,8 @@ if (Test-Path build/tap/result.tap) {
 
 # test.2.hurl is KO but we want the script to continue until the end
 $ErrorActionPreference = 'Continue'
-hurl --test --report-tap build/tap/result.tap tests_ok/test.1.hurl tests_ok/test.2.hurl
-hurl --test --report-tap build/tap/result.tap tests_ok/test.3.hurl
+hurl --test --report-tap build/tap/result.tap tests_ok/tap/test.1.hurl tests_ok/tap/test.2.hurl
+hurl --test --report-tap build/tap/result.tap tests_ok/tap/test.3.hurl
 $ErrorActionPreference = 'Stop'
 
 Write-Host (Get-Content build/tap/result.tap -Raw) -NoNewLine
