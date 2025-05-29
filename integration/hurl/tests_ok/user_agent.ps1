06756c524d093d26f1f5ee726bd9b4cc1cbdf2ea
Jean-Christophe Amiel <jeanchristophe.amiel@orange.com>
2025-05-29 10:55:23 +0200

    Move user_agent integration tests in subfolder.

--- integration/hurl/tests_ok/user_agent.ps1
+++
@@ -1,3 +0,0 @@
-Set-StrictMode -Version latest
-$ErrorActionPreference = 'Stop'
-hurl tests_ok/user_agent.hurl --user-agent "Mozilla/5.0 A"
--- integration/hurl/tests_ok/user_agent.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-set -Eeuo pipefail
-hurl tests_ok/user_agent.hurl --user-agent "Mozilla/5.0 A"
