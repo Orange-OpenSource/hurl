a23bbf5537799e6f9c294e3e7867b29aafb5ac29
Jean-Christophe Amiel <jeanchristophe.amiel@orange.com>
2025-05-29 16:34:40 +0200

    Update progress_bar integration test.

--- integration/hurl/tests_ok/progress_bar/progress_bar.ps1
+++ integration/hurl/tests_ok/progress_bar/progress_bar.ps1
@@ -1,4 +1,10 @@
 Set-StrictMode -Version latest
 $ErrorActionPreference = 'Stop'
 
-hurl --jobs 1 --progress-bar --test tests_ok/progress_bar/progress_bar_a.hurl tests_ok/progress_bar/progress_bar_b.hurl
+hurl --jobs 1 `
+    --progress-bar `
+    --test `
+    tests_ok/progress_bar/progress_bar_a.hurl `
+    tests_ok/progress_bar/progress_bar_b.hurl `
+    tests_ok/progress_bar/progress_bar_c_with_a_very_long_long_long_long_name.hurl
+
--- integration/hurl/tests_ok/progress_bar/progress_bar.sh
+++ integration/hurl/tests_ok/progress_bar/progress_bar.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 set -Eeuo pipefail
 
-hurl --jobs 1 --progress-bar --test tests_ok/progress_bar/progress_bar_a.hurl tests_ok/progress_bar/progress_bar_b.hurl
+hurl --jobs 1 \
+    --progress-bar \
+    --test \
+    tests_ok/progress_bar/progress_bar_a.hurl \
+    tests_ok/progress_bar/progress_bar_b.hurl \
+    tests_ok/progress_bar/progress_bar_c_with_a_very_long_long_long_long_name.hurl
