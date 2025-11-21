0f65955d517eaac7166b3ee40b3d4de56944eea4
Jean-Christophe Amiel <jeanchristophe.amiel@orange.com>
2025-11-21 13:53:12 +0100

    Update update_all documentation script.

--- bin/docs/update_all.sh
+++ bin/docs/update_all.sh
@@ -4,9 +4,14 @@ set -Eeuo pipefail
 # First generates Rust code, bash completion, part of hurl.md and hurlfmt.md
 python3 bin/spec/options/generate_all.py
 
-# Regenerates manual, READMEs etc..
+# Generates manual, READMEs etc..
 python3 bin/release/gen_manpage.py docs/manual/hurl.md > docs/manual/hurl.1
 python3 bin/release/gen_manpage.py docs/manual/hurlfmt.md > docs/manual/hurlfmt.1
 python3 bin/docs/build_man_md.py docs/manual/hurl.md > docs/manual.md
 python3 bin/docs/build_readme.py github > README.md
 python3 bin/docs/build_readme.py crates > packages/hurl/README.md
+
+# Generates standalone doc
+VERSION="$(grep '^version' packages/hurl/Cargo.toml | cut -f2 -d'"')"
+VERSION=${VERSION%-SNAPSHOT}
+python3 bin/docs/build_standalone_md.py > docs/standalone/hurl-"$VERSION".md
