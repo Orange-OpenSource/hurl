e00841399dce425d3e1f6ad95cb02c912e4ae703
Jean-Christophe Amiel <jeanchristophe.amiel@orange.com>
2026-01-31 10:56:18 +0100

    Use latest version of cargo-valgrind to get suppression files for Rust 1.93.

--- bin/check/valgrind.sh
+++ bin/check/valgrind.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 set -Eeuo pipefail
 
-exit 0
+valgrind --version
+cargo-valgrind --help
 
-#valgrind --version
-#cargo-valgrind --help
-#
-#cat <<END | cargo valgrind run -p hurl -- --test
-#GET https://unpkg.com/vue@3.4.27/dist/vue.global.prod.js
-#HTTP 200
-#END
+cat <<END | cargo valgrind run -p hurl -- --test
+GET https://unpkg.com/vue@3.4.27/dist/vue.global.prod.js
+HTTP 200
+END
