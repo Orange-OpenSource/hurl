a1f887137d7ea15de5e3919eeb6cd82825c2f9e7
lepapareil <filipe.pinto@orange.com>
2026-02-02 14:51:37 +0100

    Optimize checks prereqs

--- bin/check/hadolint.sh
+++ bin/check/hadolint.sh
@@ -2,8 +2,5 @@
 set -Eeuo pipefail
 
 docker_file="$1"
-version="2.12.0"
-wget --quiet --output-document /tmp/hadolint "https://github.com/hadolint/hadolint/releases/download/v${version}/hadolint-Linux-x86_64"
-chmod +x /tmp/hadolint
-/tmp/hadolint --verbose --ignore DL3018 --ignore SC1091 "${docker_file}"
+hadolint --verbose --ignore DL3018 --ignore SC1091 "${docker_file}"
 
