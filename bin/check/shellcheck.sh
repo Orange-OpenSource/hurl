4539ee77dd14ce14ae16ddd1da70ee5e98c24d4e
lepapareil <filipe.pinto@orange.com>
2026-02-08 20:48:07 +0100

    Enable shellcheck over all workflows

--- bin/check/shellcheck.sh
+++ bin/check/shellcheck.sh
@@ -4,7 +4,6 @@ set -Eeuo pipefail
 function init_colors(){
     color_red=$(echo -ne "\033[1;31m")
     color_green=$(echo -ne "\033[1;32m")
-    color_yellow=$(echo -ne "\033[1;33m")
     color_cyan=$(echo -ne "\033[1;36m")
     color_reset=$(echo -ne "\033[0m")
 }
@@ -22,13 +21,13 @@ fi
 
 function shellcheck_files(){
     echo "# shellcheck sh scripts files"
-    find . -type f -name '*.sh' -print0  | xargs -0 shellcheck
+    find . -type f -name '*.sh' -print0 | xargs -0 shellcheck
     echo "${color_green}No problem with sh script files${color_reset}"
     echo
 }
 
 function shellcheck_gitflow(){
-    echo "# shellcheck sh scripts inside Github Workflow"
+    echo "# shellcheck sh scripts inside Github Workflows"
     error_count=0
     for yaml in .github/workflows/*yml ; do
         echo "${color_cyan}  ======================================================================="
@@ -36,12 +35,11 @@ function shellcheck_gitflow(){
         echo "${color_cyan}  ======================================================================="
         echo "${color_cyan}  |${color_reset}"
         file=$(basename "${yaml}")
-	if [ "${file}" == "accept-pull-request.yml" ] || [ "${file}" == "release.yml" ] ; then
-            echo "${color_cyan}  |${color_yellow} Disabled for now because output vars have to be rewrited from scratch"
-	    echo "${color_cyan}  |${color_reset}"
-            continue
-        fi
         tmp="/tmp/${file}"
+	{
+	echo "#!/bin/bash"
+        echo "set -Eeuo pipefail"
+        } > "${tmp}"
 	yq '.jobs[] | select(.["runs-on"] | test("windows") | not).steps[] | select(.run != null) | .run' "${yaml}" \
 	    | sed -E 's/\$\{\{[[:space:]]*/\${/g' \
             | sed -E 's/[[:space:]]*\}\}/}/g' \
@@ -49,13 +47,13 @@ function shellcheck_gitflow(){
             | sed -E 's/(\$\{[^}]*)\.([^}]*)/\1-\2/g' \
             | sed -E 's/(\$\{[^}]*)\.([^}]*)/\1-\2/g' \
             | sed -E 's/(\$\{[^}]*)\.([^}]*)/\1-\2/g' \
-	    > "${tmp}"
-        if ! output=$(shellcheck --exclude=SC2148 --color=always "${tmp}" 2>&1) ; then
+	    | sed '/^[[:space:]]*exit[[:space:]]\+[01][[:space:]]*$/d' \
+	    >> "${tmp}"
+        if ! output=$(shellcheck --exclude=SC2153 --color=always "${tmp}" 2>&1) ; then
             echo "${output}" | sed "/In.*${file}.*/d" | sed "s/^/${color_cyan}  |${color_reset}  /g"
             error_count=$((error_count+1))
 	else
 	    echo "${color_cyan}  | ${color_green}No problem with ${yaml}${color_reset}"
-	    echo "${color_cyan}  |"
         fi
         rm "${tmp}"
         echo
