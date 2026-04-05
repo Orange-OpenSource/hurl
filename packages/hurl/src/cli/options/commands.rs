f00ade941dd82e3b0b8cc90796ec001877ac1561
Manuel Sanchez Pinar <aomanu@gmail.com>
2026-04-05 16:13:39 +0200

    Update help for --jobs (minimum value is 1)

--- packages/hurl/src/cli/options/commands.rs
+++ packages/hurl/src/cli/options/commands.rs
@@ -294,7 +294,7 @@ pub fn jobs() -> clap::Arg {
         .long("jobs")
         .value_name("NUM")
         .value_parser(clap::value_parser!(u32).range(1..))
-        .help("Maximum number of parallel jobs, 0 to disable parallel execution")
+        .help("Maximum number of parallel jobs, 1 to disable parallel execution")
         .help_heading("Run options")
         .num_args(1)
 }
