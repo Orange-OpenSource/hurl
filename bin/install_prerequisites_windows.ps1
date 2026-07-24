a2c72ca7a34dcc97950a6abe80dbafe6dd2b37fb
Filipe PINTO <filipe.pinto@orange.com>
2026-07-24 11:29:40 +0200

    Fix Squid installation on Windows

--- bin/install_prerequisites_windows.ps1
+++ bin/install_prerequisites_windows.ps1
@@ -3,31 +3,28 @@ $ErrorActionPreference = 'Stop'
 
 write-host -foregroundcolor Cyan "----- install system prerequisites -----"
 
-# update vcpkg install
-$vcpkg_dir=((Get-command vcpkg).Source | Split-Path)
-$lib_dir="$vcpkg_dir\installed\x64-windows\bin"
-& "$vcpkg_dir\bootstrap-vcpkg.bat"
-# Downgrade to 8.19.0 => https://github.com/Orange-OpenSource/hurl/issues/5105
-git -C $vcpkg_dir checkout 4f326c4072038c8624c36a8ba5ed23f616adda53
-
-# install libxml and libcurl
-vcpkg install --recurse curl[core,sspi,http2,non-http,ssl]:x64-windows
-vcpkg install --recurse libxml2[core,iconv]:x64-windows
-
-vcpkg update
-if ($LASTEXITCODE) { Throw }
-vcpkg upgrade --no-dry-run
-if ($LASTEXITCODE) { Throw }
-vcpkg integrate install
-if ($LASTEXITCODE) { Throw }
-
 # install python 3.11
 choco install --confirm python311
 if ($LASTEXITCODE) { Throw }
 
 # install proxy
 echo "==== install Squid"
-choco install --confirm squid --install-arguments="'TARGETDIR=C:\'"
+# Fix broken Chocolatey Squid package 4.14 because http://packages.diladele.com/squid/4.14/squid.msi moved to https://www.diladele.com/pkg/squid/4.14/squid.msi
+Invoke-WebRequest https://community.chocolatey.org/api/v2/package/squid -OutFile squid.nupkg
+Rename-Item squid.nupkg choco-squid.zip
+Expand-Archive choco-squid.zip choco-squid
+Select-String -Path .\choco-squid\tools\chocolateyinstall.ps1 -Pattern "diladele"
+$file = '.\choco-squid\tools\chocolateyinstall.ps1'
+$content = Get-Content $file -Raw
+$content = $content.Replace(
+    'http://packages.diladele.com/squid/4.14/squid.msi',
+    'https://www.diladele.com/pkg/squid/4.14/squid.msi'
+)
+Set-Content -Path $file -Value $content -Encoding UTF8
+cd .\choco-squid
+choco pack
+cd ..
+choco install --debug --confirm squid --source=".\choco-squid" --install-arguments="'TARGETDIR=C:\'"
 if ($LASTEXITCODE) { Throw }
 echo "==== create log dir integration\build"
 New-Item -ItemType Directory -Path integration\build -Force
@@ -50,4 +47,22 @@ Get-Process | Where {$_.Name -eq "Squid"} | tee -Append -filepath integration\bu
 # install jq
 echo "==== install jq"
 choco install --confirm jq
+if ($LASTEXITCODE) { Throw }
+
+# update vcpkg install
+$vcpkg_dir=((Get-command vcpkg).Source | Split-Path)
+$lib_dir="$vcpkg_dir\installed\x64-windows\bin"
+& "$vcpkg_dir\bootstrap-vcpkg.bat"
+# Downgrade to 8.19.0 => https://github.com/Orange-OpenSource/hurl/issues/5105
+git -C $vcpkg_dir checkout 4f326c4072038c8624c36a8ba5ed23f616adda53
+
+# install libxml and libcurl
+vcpkg install --recurse curl[core,sspi,http2,non-http,ssl]:x64-windows
+vcpkg install --recurse libxml2[core,iconv]:x64-windows
+
+vcpkg update
+if ($LASTEXITCODE) { Throw }
+vcpkg upgrade --no-dry-run
+if ($LASTEXITCODE) { Throw }
+vcpkg integrate install
 if ($LASTEXITCODE) { Throw }
