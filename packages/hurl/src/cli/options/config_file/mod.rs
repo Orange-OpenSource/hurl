6a1321c1ee6ff9e71392b7892c66c80dd5560f76
Fabrice Reix <fabrice.reix@orange.com>
2026-08-14 16:01:49 +0200

    Add option proxy_header in config_file

--- packages/hurl/src/cli/options/config_file/mod.rs
+++ packages/hurl/src/cli/options/config_file/mod.rs
@@ -341,6 +341,19 @@ fn parse_option(reader: &mut Reader, options: &mut CliOptions) -> Result<(), Con
             options.no_proxy = Some(value);
             Ok(())
         }
+        "proxy-header" => {
+            parse_value_separator(reader)?;
+            let value = parse_value(reader)?;
+
+            if value.is_empty() {
+                return Err(ConfigFileError::new(
+                    save.pos,
+                    "Option --proxy-header requires a value",
+                ));
+            }
+            options.proxy_headers.push(value);
+            Ok(())
+        }
         "no-jsonpath-coercion" => {
             expect_no_value(reader)?;
             options.no_jsonpath_coercion = true;
@@ -847,6 +860,16 @@ mod tests {
         assert!(options.no_proxy.is_none());
         assert!(parse_option(&mut reader, &mut options).is_ok());
         assert_eq!(options.no_proxy, Some("127.0.0.1".to_string()));
+        assert_eq!(reader.cursor().pos, Pos::new(2, 1));
+    }
+
+    #[test]
+    fn test_parse_option_proxy_header() {
+        let mut reader = Reader::new("--proxy-header=From-Proxy:Hello\n");
+        let mut options = CliOptions::default();
+        assert!(options.proxy_headers.is_empty());
+        assert!(parse_option(&mut reader, &mut options).is_ok());
+        assert_eq!(options.proxy_headers, vec!["From-Proxy:Hello"]);
         assert_eq!(reader.cursor().pos, Pos::new(2, 1));
     }
 
